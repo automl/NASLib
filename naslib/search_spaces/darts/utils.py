d5f999333517acfcf45be674a8bd23643609bcde
Arber Zela <zelaa@cs.uni-freiburg.de>
2020-03-21 18:13:21 +0100

    added base darts folder and ops

---
+++ naslib/search_spaces/darts/utils.py
@@ -0,0 +1,39 @@
+import itertools
+import os
+import re
+import collections
+import glob
+import json
+import pickle
+
+import networkx as nx
+import numpy as np
+import scipy.stats as stats
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from naslib.search_spaces.core.operations import *
+
+
+OPS = {
+  'none' : lambda C, stride, affine: Zero(stride),
+  'avg_pool_3x3' : lambda C, stride, affine: nn.AvgPool2d(3, stride=stride, padding=1, count_include_pad=False),
+  'max_pool_3x3' : lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
+  'skip_connect' : lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
+  'sep_conv_3x3' : lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
+  'sep_conv_5x5' : lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
+  'sep_conv_7x7' : lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
+  'dil_conv_3x3' : lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
+  'dil_conv_5x5' : lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
+  'conv_7x1_1x7' : lambda C, stride, affine: nn.Sequential(
+    nn.ReLU(inplace=False),
+    nn.Conv2d(C, C, (1,7), stride=(1, stride), padding=(0, 3), bias=False),
+    nn.Conv2d(C, C, (7,1), stride=(stride, 1), padding=(3, 0), bias=False),
+    nn.BatchNorm2d(C, affine=affine)
+    ),
+}
+
+PRIMITIVES = list(OPS.keys())
+
+
