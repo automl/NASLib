f8b785b06129cbfcba06f88a79ce2c0a7aadea20
Arber Zela <zelaa@cs.uni-freiburg.de>
2020-05-10 15:21:53 +0200

    fixed runners in darts and nasbench201

--- naslib/benchmarks/darts/runner.py
+++ naslib/benchmarks/darts/runner.py
@@ -1,10 +1,26 @@
+import logging
+import os
+import sys
+
 from naslib.search_spaces.darts import MacroGraph, PRIMITIVES, OPS
+from naslib.optimizers.core import NASOptimizer, Evaluator
 from naslib.optimizers.oneshot.darts import DARTSOptimizer, Searcher
 from naslib.utils import config_parser
+from naslib.utils.parser import Parser
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+format=log_format, datefmt='%m/%d %I:%M:%S %p')
 
 
 if __name__ == '__main__':
     config = config_parser('../../configs/default.yaml')
+    parser = Parser('../../configs/default.yaml')
+
+    fh = logging.FileHandler(os.path.join(parser.config.save,
+                                      'log_{}.txt'.format(config.seed)))
+    fh.setFormatter(logging.Formatter(log_format))
+    logging.getLogger().addHandler(fh)
 
     one_shot_optimizer = DARTSOptimizer.from_config(**config)
     search_space = MacroGraph.from_optimizer_op(
@@ -17,4 +33,15 @@ if __name__ == '__main__':
 
     searcher = Searcher(search_space, arch_optimizer=one_shot_optimizer)
     searcher.run()
+
+    # discretize
+    final_arch = search_space.discretize(n_input_edges=[2 for _ in search_space.inter_nodes()])
+    del search_space, one_shot_optimizer, searcher
+
+    # run final network from scratch
+    opt = NASOptimizer()
+    final_arch.parse(opt)
+    evaluator = Evaluator(final_arch, parser)
+    evaluator.run()
+
 
