8821be89c0acfeaf245bc7a45937bc83152ef954
JulienSiems <juliensiems@gmail.com>
2020-05-11 11:26:07 +0200

    add parameters for gumbel softmax temperature also to nasbench 201 config.

--- naslib/benchmarks/nasbench201/runner.py
+++ naslib/benchmarks/nasbench201/runner.py
@@ -3,26 +3,25 @@ import logging
 import os
 import sys
 
-from naslib.optimizers.oneshot import Searcher
-from naslib.optimizers.oneshot.darts import DARTSOptimizer
-from naslib.optimizers.core import NASOptimizer, Evaluator
+from naslib.optimizers.oneshot.darts import Searcher, DARTSOptimizer
+from naslib.optimizers.oneshot.gdas import GDASOptimizer
 from naslib.search_spaces.nasbench201 import MacroGraph, PRIMITIVES, OPS
 from naslib.utils import config_parser
-from naslib.utils.utils import create_exp_dir
 from naslib.utils.parser import Parser
+from naslib.utils.utils import create_exp_dir
+
+opt_list = [DARTSOptimizer, GDASOptimizer]
 
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-format=log_format, datefmt='%m/%d %I:%M:%S %p')
-
+                    format=log_format, datefmt='%m/%d %I:%M:%S %p')
 
 parser = argparse.ArgumentParser('nasbench201')
-parser.add_argument('--optimizer', type=str, default='DARTSOptimizer')
+parser.add_argument('--optimizer', type=str, default='GDASOptimizer')
 parser.add_argument('--seed', type=int, default=1)
 parser.add_argument('--dataset', type=str, default='cifar10')
 parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
 args = parser.parse_args()
-
 
 if __name__ == '__main__':
     config = config_parser('../../configs/nasbench_201.yaml')
@@ -33,7 +32,7 @@ if __name__ == '__main__':
     create_exp_dir(parser.config.save)
 
     fh = logging.FileHandler(os.path.join(parser.config.save,
-                                      'log_{}.txt'.format(config.seed)))
+                                          'log_{}.txt'.format(config.seed)))
     fh.setFormatter(logging.Formatter(log_format))
     logging.getLogger().addHandler(fh)
 
@@ -48,4 +47,3 @@ if __name__ == '__main__':
 
     searcher = Searcher(search_space, parser, arch_optimizer=one_shot_optimizer)
     searcher.run()
-
