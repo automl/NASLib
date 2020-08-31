233930537ca0ac423945b3447fb25afc48619dc2
ruchtem <34424489+ruchtem@users.noreply.github.com>
2020-08-31 07:21:36 +0200

    added skeleton of sspace notebook

--- naslib/benchmarks/test/test.py
+++ naslib/benchmarks/test/test.py
@@ -1,20 +1,14 @@
-import argparse
 import logging
-import os
-import sys
-import torch
-import numpy as np
-import random
 
-from naslib.optimizers.discrete.rs import RandomSearch
-from naslib.optimizers.core import NASOptimizer, Evaluator, Trainer
-from naslib.optimizers.oneshot.darts import Searcher, DARTSOptimizer
+from naslib.optimizers.core import Trainer
+from naslib.optimizers.oneshot.darts import DARTSOptimizer
 from naslib.optimizers.oneshot.gdas import GDASOptimizer
-from naslib.optimizers.oneshot.pc_darts import PCDARTSOptimizer
-from naslib.search_spaces.darts import MacroGraph, PRIMITIVES, OPS, DartsSearchSpace, SimpleCellSearchSpace
+from naslib.optimizers.discrete.rs import RandomSearch
+
+from naslib.search_spaces.darts import DartsSearchSpace, SimpleCellSearchSpace
 from naslib.utils import config_parser, set_seed
 from naslib.utils.parser import Parser
-from naslib.utils.utils import create_exp_dir
+
 
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
