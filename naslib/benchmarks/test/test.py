6ceed1584d5b17b881e9f29d2dbe58cbc62b0e6c
ruchtem <34424489+ruchtem@users.noreply.github.com>
2020-09-10 14:08:17 +0200

    added sspace for nasbench201

--- naslib/benchmarks/test/test.py
+++ naslib/benchmarks/test/test.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import naslib as nl
 
 from naslib.optimizers.core.evaluator import Trainer
 from naslib.optimizers.oneshot.darts.optimizer import DARTSOptimizer
@@ -8,19 +9,29 @@ from naslib.optimizers.discrete.rs.optimizer import RandomSearch
 
 from naslib.search_spaces.cell.darts import DartsSearchSpace
 from naslib.search_spaces.cell.simple import SimpleCellSearchSpace
+from naslib.search_spaces.nasbench201.nasbench201 import NasBench201SeachSpace
 from naslib.utils import utils
 from naslib.utils.logging import setup_logger
 
 logger = setup_logger("test.log")
-logger.setLevel(logging.INFO)
+#logger.setLevel(logging.INFO)
+
+
+import types
+from naslib.search_spaces.core.primitives import *
+
+import networkx as nx
 
 if __name__ == '__main__':
     
     config = utils.get_config_from_args()
     utils.set_seed(config.seed)
 
-    search_space = DartsSearchSpace()
+    # search_space = DartsSearchSpace()
     # search_space = SimpleCellSearchSpace()
+    search_space = NasBench201SeachSpace()
+
+    
 
     # optimizer = RandomSearch(sample_size=1)
     optimizer = DARTSOptimizer(config)
@@ -29,5 +40,5 @@ if __name__ == '__main__':
     optimizer.adapt_search_space(search_space)
     
     trainer = Trainer(optimizer, 'cifar10', config)
-    trainer.train()
+    trainer.search()
     trainer.evaluate()
--- naslib/search_spaces/nasbench201/__init__.py
+++
@@ -1,2 +0,0 @@
-from naslib.search_spaces.nasbench201.graph import Cell, MacroGraph
-from naslib.search_spaces.nasbench201.primitives import OPS, PRIMITIVES
