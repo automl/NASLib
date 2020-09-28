81a34e7626bbae320e18efc6274b6aa109073692
ruchtem <34424489+ruchtem@users.noreply.github.com>
2020-09-28 14:55:51 +0200

    some analysis modifications

--- naslib/analysis/evaluate_nasbench_201.py
+++ naslib/analysis/evaluate_nasbench_201.py
@@ -14,7 +14,7 @@ rcParams.update({'font.size': 12})
 
 optimizer_dir_name_to_name = {
     'DARTS': 'DARTS', 'GDAS': 'GDAS', 'PCDARTS': 'PC-DARTS', 'SDARTSDARTS': 'Smooth-DARTS',
-    'SDARTSPCDARTS': 'Smooth-PC-DARTS', 'SDARTSGDAS': 'Smooth-GDAS'
+    'SDARTSPCDARTS': 'Smooth-PC-DARTS', 'SDARTSGDAS': 'Smooth-GDAS', 'gdas': 'GDAS', 'darts': 'darts'
 }
 
 markers={
@@ -38,8 +38,9 @@ def get_nb_eval(optimizer_runs, dataset, metric):
     nb_metric_per_run = []
     for run in optimizer_runs:
         nb_metric = []
-        for eval in run['arch_eval']:
-            nb_metric.append(eval[dataset][metric])
+        nb_metric = run[metric]
+        #for eval in run['arch_eval']:
+        #    nb_metric.append(eval[dataset][metric])
         nb_metric_per_run.append(nb_metric)
     return nb_metric_per_run
 
@@ -47,46 +48,53 @@ def get_nb_eval(optimizer_runs, dataset, metric):
 def analyze(optimizer_dict, dataset):
     fig, ax_left = plt.subplots(figsize=(6, 4))
 
+    lines = []
     for optimizer_name, optimizer_runs in optimizer_dict.items():
         nb_test_error = 100 - np.array(get_nb_eval(optimizer_runs, dataset,
-                                                 'test_accuracy'))
+                                                 'test_acc'))
 
         mean, std = np.mean(nb_test_error, axis=0), np.std(nb_test_error, axis=0)
-        ax_left.plot(np.arange(len(mean)), mean,
-                     label=optimizer_dir_name_to_name[optimizer_name],
+        line = ax_left.plot(np.arange(len(mean)), mean,
+                     label="{} test".format(optimizer_dir_name_to_name[optimizer_name]),
                      marker=markers.get(optimizer_name, None),
                      markersize=10, markevery=(0.1,0.1))
         ax_left.fill_between(np.arange(len(mean)), mean - std, mean + std, alpha=0.3)
+        lines.append(line)
 
     ax_left.set_xlabel('Search Epochs')
     ax_left.set_ylabel('Test Error [%]')
     plt.title(dataset)
-    plt.legend()
     plt.grid(True, which="both", ls="-", alpha=0.8)
+    ax_left.legend()
 
     ax_right = ax_left.twinx()  # instantiate a second axes that shares the same x-axis
     ax_right.set_ylabel('Validation Error [%]')
+    
     for optimizer_name, optimizer_runs in optimizer_dict.items():
         one_shot_valid_error = 100 - np.array([run['valid_acc'] for run in
                                              optimizer_runs])
         mean, std = np.mean(one_shot_valid_error, axis=0), np.std(one_shot_valid_error, axis=0)
-        ax_right.plot(np.arange(len(mean)), mean, linestyle='-.', alpha=0.3)
+        line = ax_right.plot(np.arange(len(mean)), mean, 
+            label="{} valid".format(optimizer_dir_name_to_name[optimizer_name]),
+            linestyle='-.', 
+            alpha=0.3)
         ax_right.fill_between(np.arange(len(mean)), mean - std, mean + std, linestyle=':', alpha=0.1)
+        lines.append(line)
 
     #ax_left.set_yscale('log')
     #ax_right.set_yscale('log')
     #plt.xlim(left=0, right=len(mean))
+    ax_left.legend(lines, [l.get_label() for l in lines])
     plt.tight_layout()
     plt.savefig('optimizer_comp_nb201_{}.pdf'.format(dataset))
 
 
 if __name__ == '__main__':
     # optimizer_dict = {'DARTS': [], 'GDAS': [], 'PCDARTS': []}
-    optimizer_dict = {'SDARTSDARTS': [], 'SDARTSGDAS': [], 'SDARTSPCDARTS': []}
+    optimizer_dict = {'gdas': [], 'darts': []}
     for optimizer in optimizer_dict.keys():
-        optimizer_path = '/home/zelaa/NASLib/naslib/benchmarks/nasbench201/run/cifar10/{}Optimizer'.format(
-            optimizer)
-        for res_json_path in glob.glob(os.path.join(optimizer_path, 'errors_*.json')):
+        optimizer_path = '/home/moa/dev/python_projects/NASLib/naslib/benchmarks/nasbench201/run/cifar10/{}/*/'.format(optimizer)
+        for res_json_path in glob.glob(os.path.join(optimizer_path, 'errors.json')):
             optimizer_dict[optimizer].append(json.load(open(res_json_path, 'r')))
 
     for dataset in ['cifar10', 'cifar100', 'ImageNet16-120']:
---
+++ naslib/analysis/query_with_checkpoints.py
@@ -0,0 +1,67 @@
+import logging
+import sys
+import glob
+import os
+
+from fvcore.common.checkpoint import Checkpointer
+
+from naslib.defaults.trainer import Trainer
+from naslib.optimizers import DARTSOptimizer, GDASOptimizer, RandomSearch
+from naslib.optimizers.discrete.re.optimizer import RegularizedEvolution
+
+from naslib.search_spaces import (
+    DartsSearchSpace, 
+    SimpleCellSearchSpace, 
+    NasBench201SeachSpace, 
+    HierarchicalSearchSpace,
+)
+
+from naslib.utils import utils, setup_logger
+
+# Read args and config, setup logger
+config = utils.get_config_from_args()
+utils.set_seed(config.seed)
+
+logger = setup_logger(config.save + "/log.log")
+logger.setLevel(logging.INFO)   # default DEBUG is too verbose
+
+utils.log_args(config)
+
+supported_optimizers = {
+    'darts': DARTSOptimizer(config.search),
+    'gdas': GDASOptimizer(config.search),
+    'random': RandomSearch(sample_size=1),
+    #'re': RegularizedEvolution(config.search),
+}
+
+
+# search_space = SimpleCellSearchSpace()
+search_space = NasBench201SeachSpace()
+# search_space = HierarchicalSearchSpace()
+# search_space = DartsSearchSpace()
+
+assert search_space.QUERYABLE
+
+optimizer = supported_optimizers[config.optimizer]
+
+optimizer.adapt_search_space(search_space)
+
+checkpoint_dir = '/home/moa/dev/python_projects/NASLib/naslib/benchmarks/nasbench201/run/cifar10/{}/4/search/'.format(config.optimizer)
+checkpointables = optimizer.get_checkpointables()
+
+checkpointer = Checkpointer(
+    model=checkpointables.pop('model'),
+    save_dir="/tmp/",
+    **checkpointables
+)
+
+for checkpoint in sorted(glob.glob(os.path.join(checkpoint_dir, 'model_0*.pth'))):
+
+    checkpoint = checkpointer.resume_or_load(checkpoint, resume=False)
+    epoch = checkpoint.get("iteration", -1)
+    
+    print(optimizer.test_statistics())
+
+
+
+trainer.evaluate(resume_from=checkpoint)
