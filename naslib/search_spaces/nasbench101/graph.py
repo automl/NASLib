498d9c2e13817405bd790de79181c5f20c89f5c2
mehtay <mehtay@informatik.uni-freiburg.de>
2020-10-01 18:41:25 +0200

    skeleton for nb101 interface ready!

--- naslib/search_spaces/nasbench101/graph.py
+++ naslib/search_spaces/nasbench101/graph.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import torch.nn as nn
+import numpy as np
 
 from naslib.search_spaces.core import primitives as ops
 from naslib.search_spaces.core.graph import Graph, EdgeData
@@ -41,103 +42,49 @@ class NasBench101SeachSpace(Graph):
         #
         # Cell definition
         #
-        cell = Graph()
-        cell.name = "cell"    # Use the same name for all cells with shared attributes
+        node_pair = Graph()
+        node_pair.name = "node_pair"    # Use the same name for all cells with shared attributes
 
+        # need to add subgraphs on the nodes, each subgraph has option for 3 ops
         # Input node
-        cell.add_node(1)
+        node_pair.add_node(1)
+        node_pair.add_node(2)
+        node_pair.add_edges_from[(1,2)]
 
-        # Intermediate nodes
-        for i in range(2, 12):
-            cell.add_node(i)
+        cell = Graph()
         
-        # Output node
-        cell.add_node(12)
+        cell.add_node(1)    # input node
+        #* question: what is set_input doing here, when we are defining the edges later?
+        cell.add_node(2, subgraph=node_pair.set_scope("stack_1").set_input([1]))
+        cell.add_node(3, subgraph=node_pair.copy().set_scope("stack_1"))
+        cell.add_node(4, subgraph=node_pair.copy().set_scope("stack_1"))
+        cell.add_node(5, subgraph=node_pair.copy().set_scope("stack_1"))
+        cell.add_node(6, subgraph=node_pair.copy().set_scope("stack_1"))
+        cell.add_node(7)    # output
 
         # Edges
-        pair_edges = []
-        edges = []
-        nodes = sorted(list(self.nodes()))
-
-        # create a pair of nodes so that the edge can have options for: [1x1, 3x3, MP] operations (type1 edge)
-        for i in range(2, 12, 2):
-            pair_edges.append((i, i+1))
-
-        # connect output of each pair to the input of the forward ones: [Identity, Zero] (type2 edge)
-        for i in range(3, 12, 2):
-            for j in range(i+1, 11, 2):
-                edges.append((i, j))
-        
-        cell.add_edges_from(pair_edges)
-        cell.add_edges_from(edges)
+        cell.add_edges_densly()
 
         #
         # dummy Makrograph definition for RE for benchmark queries
         #
-        self.add_nodes_from([1, 2])
-        self.add_edges_from[(1,2)]
-        
         channels = [128, 256, 512]
-        self.edges[1, 2].set('op', ops.Stem(channels[0]))
-        self.edges[2, 3].set('op', cell.copy().set_scope('stack_1'))
-        
         self.name = "makrograph"
-
-        #
-        # Makrograph definition
-        #
-
-        # Cell is on the edges
-        # 1-2:                  preprocessing: stem (128c, 3x3 conv)   
-        # 2-3, 3-4, 4-5:        cells stack 1 
-        # 5-6:                  downsample (img (h, w) 0.5x, channels 2x)
-        # 6-7, 7-8, 8-9:        cells stack 2
-        # 9-10:                 downsample (img (h, w) 0.5x, channels 2x)
-        # 10-11, 11-12, 12-13:  cells stack 3          
-        # 13-14:                post-processing:  global avg pool + fc
-
-        # total_num_nodes = 14
+        self.edges[1, 2].set('op', ops.Stem(channels[0]))
+        self.edges[2, 3].set('op', cell.copy().set_scope('stage_1'))
         
-        # self.add_nodes_from(range(1, total_num_nodes+1))
-        # self.add_edges_from([(i, i+1) for i in range(1, total_num_nodes)])
-
-        # channels = [128, 256, 512]
-
-        #
-        # operations at the edges
-        #
-
-        # preprocessing
-        # self.edges[1, 2].set('op', ops.Stem(channels[0]))
         
-        # stage 1
-        # for i in range(2, 5):
-            # self.edges[i, i+1].set('op', cell.copy().set_scope('stack_1'))
+        node_pair.update_edges(
+            update_func=lambda current_edge_data: _set_node_ops(current_edge_data, C=channels[0]),
+            scope="node",
+            private_edge_data=True
+        )
         
-        # stage 2
-        # self.edges[5, 6].set('op', ops.MaxPool1x1(C_in=channels[0], C_out=channels[1], stride=2))
-        # for i in range(6, 9):
-        #     self.edges[i, i+1].set('op', cell.copy().set_scope('stack_2'))
-
-        # stage 3
-        # self.edges[9, 10].set('op', MaxPool1x1(C_in=channels[1], C_out=channels[2], stride=2))
-        # for i in range(10, 13):
-        #     self.edges[i, i+1].set('op', cell.copy().set_scope('stack_3'))
-
-        # post-processing
-        # self.edges[13, 14].set('op', ops.Sequential(
-        #     nn.AdaptiveAvgPool2d(1),
-        #     nn.Flatten(),
-        #     nn.Linear(channels[-1], 10)
-        # ))
-        
-        # set the ops at the cells (channel dependent)
-        for c, scope in zip(channels, self.OPTIMIZER_SCOPE):
-            self.update_edges(
-                update_func=lambda current_edge_data: _set_cell_ops(current_edge_data, C=c),
-                scope=scope,
-                private_edge_data=True
-            )
+        cell.update_edges(
+            update_func=lambda current_edge_data: _set_cell_ops(current_edge_data, C=channels[0]),
+            scope="cell",
+            private_edge_data=True
+        )
 
     def query(self, metric=None, dataset='cifar10', path=None):
         """
@@ -159,8 +106,8 @@ class NasBench101SeachSpace(Graph):
         # convert the naslib representation to nasbench101
         nb101_spec = _convert_cell_to_nb101_spec(cell)        
     
-        if not nasbench.is_valid(spec):
-            return 'invalid' # or some negative reward.
+        if not nasbench.is_valid(nb101_spec):
+            return 'invalid' # or some negative reward or none
 
         query_results = nasbench.query(nb101_spec)
 
@@ -168,6 +115,34 @@ class NasBench101SeachSpace(Graph):
             return query_results
             
         return query_results[metric_to_nb101[metric]]
+
+def _convert_cell_to_nb101_spec(cell):
+    
+    matrix = np.triu(np.ones((7,7)), 1)
+
+    ops_to_nb101 = {
+            'MaxPool1x1': 'maxpool3x3',
+            'ReLUConvBN1x1': 'conv1x1-bn-relu',
+            'ReLUConvBN3x3': 'conv3x3-bn-relu',
+        }
+
+    ops_to_nb101_edges = {
+        'Identity': 1,
+        'Zero': 0,
+    }
+
+    num_vertices = 7
+    ops = ['input'] * num_vertices
+    ops[-1] = 'output'
+
+    for i in range(1, 6):
+        ops[i] = ops_to_nb101[cell.nodes[i+1]['subgraph'].edges[1, 2]['op'].get_op_name]
+    
+    matrix[i][j] = ops_to_nb101_edges[cell.edges[i, j]['op'].get_op_name] for i, j in cell.edges
+    
+    spec = api.ModelSpec(matrix=matrix, ops=ops)
+    
+    return spec
 
 
 def _set_pair_ops(current_edge_data, C):
@@ -183,26 +158,3 @@ def _set_cell_ops(current_edge_data, C):
         ops.Identity(),
         ops.Zero(stride=1), 
     ])
-
-def _convert_cell_to_nb101_spec(cell):
-    
-    # matrix = nx.adjacency_matrix(cell)
-    matrix = np.triu(np.ones((7,7)), 1)
-
-    ops_to_nb101 = {
-            'MaxPool1x1': 'maxpool3x3',
-            'ReLUConvBN1x1': 'conv1x1-bn-relu',
-            'ReLUConvBN3x3': 'conv3x3-bn-relu',
-        }
-
-    edge_op_dict = {
-            (i, j): ops_to_nb101[cell.edges[i, j]['op'].get_op_name] for i, j in cell.edges
-        }
-
-    num_vertices = 7
-    ops = ['input'] * num_vertices
-    ops[-1] = 'output'
-
-    spec = api.ModelSpec(matrix=matrix, ops=ops)
-    
-    return spec
