fa10592c46e3c299fd33c123153018e56caa75ec
ruchtem <34424489+ruchtem@users.noreply.github.com>
2020-10-01 23:55:09 +0200

    Updated readme

--- examples/run_darts.py
+++ examples/run_darts.py
@@ -1,26 +1,23 @@
 import logging
 
-from naslib.optimizers.core.evaluator import Trainer
+from naslib.defaults.trainer import Trainer
 from naslib.optimizers import DARTSOptimizer, GDASOptimizer, RandomSearch
 from naslib.search_spaces import DartsSearchSpace, SimpleCellSearchSpace
 
 from naslib.utils import set_seed, setup_logger, get_config_from_args
 
-logger = setup_logger("example.log")
-
-#
-# Run as `python run_darts.py --config-file ../naslib/configs/default.yaml data ./`
-#
-
 config = get_config_from_args()     # use --help so see the options
 set_seed(config.seed)
 
-search_space = SimpleCellSearchSpace()
+logger = setup_logger(config.save + "/log.log")
+logger.setLevel(logging.INFO)   # default DEBUG is very verbose
+
+search_space = DartsSearchSpace()   # use SimpleCellSearchSpace() for less heavy search
 
 optimizer = DARTSOptimizer(config)
 optimizer.adapt_search_space(search_space)
 
-trainer = Trainer(optimizer, 'cifar10', config)
+trainer = Trainer(optimizer, config)
 trainer.search()        # Search for an architecture
 trainer.evaluate()      # Evaluate the best architecture
 
