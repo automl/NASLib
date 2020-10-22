26d69c21c566e64afa5f97febef81bd258b7d911
ruchtem <34424489+ruchtem@users.noreply.github.com>
2020-10-22 14:38:49 +0200

    increase channels in preprocessing

--- naslib/search_spaces/darts/graph.py
+++ naslib/search_spaces/darts/graph.py
@@ -15,25 +15,6 @@ from .primitives import FactorizedReduce
 logger = logging.getLogger(__name__)
 
 
-edge_attributes = {
-    'op': [
-            FactorizedReduce,   # classes of op, not instances
-            ops.Zero, 
-            ops.MaxPool,
-            ops.AvgPool, 
-            ops.SepConv, 
-            ops.SepConv, 
-            ops.DilConv,
-            ops.DilConv,
-        ],
-    'stride': 1,
-    'kernel_size': [None, None, 3, 3, 3, 5, 3, 5],      # if different for each op specify full list
-    'padding': [None, None, None, None, 1, 2, 2, 4],
-    'dilation': 2,
-    'affine': False
-}
-
-
 class DartsSearchSpace(Graph):
     """
     The search space for CIFAR-10 as defined in
@@ -54,31 +35,52 @@ class DartsSearchSpace(Graph):
     The architecture optimizer should consider all of them equally.
     """
     OPTIMIZER_SCOPE = [
-        "stage_1",
-        "stage_2", 
-        "stage_3", 
+        "n_stage_1",
+        "n_stage_2", 
+        "n_stage_3", 
+        "r_stage_1", 
+        "r_stage_2",
     ]
 
     QUERYABLE = False
 
-    def __init__(self, classes: int = 10, channels: list = [16, 32, 64]):
+    def __init__(self):
         """
         Initialize a new instance of the DARTS search space.
-
         Note:
             __init__ cannot take any parameters due to the way networkx is implemented.
             If we want to change the number of classes set a static attribute `NUM_CLASSES`
             before initializing the class. Default is 10 as for cifar-10.
         """
-        assert len(channels) == len(self.OPTIMIZER_SCOPE), \
-            "Expecting a channel for each scope. Expected {}, got {}.".format(len(self.OPTIMIZER_SCOPE), len(channels))
         super().__init__()
 
-        self.num_classes = classes
+        self.channels = [16, 32, 64]
+
+        self.num_classes = self.NUM_CLASSES if hasattr(self, 'NUM_CLASSES') else 10
+        
+        """
+        Build the search space with the parameters specified in __init__.
+        """
+        #
+        # Cell definition
+        #
 
         # Normal cell first
-        normal_cell = Graph(name="normal_cell") # Use the same name for all cells with shared attributes
-        normal_cell.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
+        normal_cell = Graph()
+        normal_cell.name = "normal_cell"    # Use the same name for all cells with shared attributes
+
+        # Input nodes
+        normal_cell.add_node(1)
+        normal_cell.add_node(2)
+
+        # Intermediate nodes
+        normal_cell.add_node(3)
+        normal_cell.add_node(4)
+        normal_cell.add_node(5)
+        normal_cell.add_node(6)
+
+        # Output node
+        normal_cell.add_node(7)
         
         # Edges
         normal_cell.add_edges_from([(1, i) for i in range(3, 7)])   # input 1
@@ -90,120 +92,98 @@ class DartsSearchSpace(Graph):
         # Edges connecting to the output are always the identity
         normal_cell.add_edges_from([(i, 7, EdgeData().finalize()) for i in range(3, 7)])   # output
         
-         # set the parameters for the ops at all edges (that are not final)
-        for k, v in edge_attributes.items():
-            normal_cell.set_at_edges(k, v)
-
-        normal_cell.nodes[7]['comb_op'] = channel_concat
-
-
         # Reduction cell has the same topology
-        reduction_cell = normal_cell.clone()
+        reduction_cell = deepcopy(normal_cell)
         reduction_cell.name = "reduction_cell"
-        reduction_cell.update_edges(
-            lambda edge: edge.data.set('stride', 2) if edge.head in [1, 2] else None
-        )
 
-        
-        # Macrograph definition
-        self.build_macrograph(channels, cells=(normal_cell, reduction_cell))
-
-        def set_channels(edge, C):
-            edge.data.set('C_in', C)
-            edge.data.set('C_out', C)
-
-        for scope, c in zip(self.OPTIMIZER_SCOPE, channels):
-            self.update_edges(lambda edge: set_channels(edge, c), scope, private_edge_data=True)
-
-
-        self.compile()
-
-
-    def build_macrograph(self, channels, cells, cells_per_stage=2, affine=False, auxiliary=False):
-
-        normal, reduction = cells
-        
-        stem_multiplier = 3
-        num_intermediate_nodes = 4
-
+        #
+        # Makrograph definition
+        #
         self.name = "makrograph"
+
         self.add_node(1)    # input node
         self.add_node(2)    # preprocessing
-        self.add_edge(1, 2, op=ops.Stem(channels[0] * stem_multiplier))     # pre-processing (stem)
+        self.add_node(3, subgraph=normal_cell.set_scope("n_stage_1").set_input([2, 2]))
+        self.add_node(4, subgraph=normal_cell.copy().set_scope("n_stage_1").set_input([2, 3]))
+        self.add_node(5, subgraph=reduction_cell.set_scope("r_stage_1").set_input([3, 4]))
+        self.add_node(6, subgraph=normal_cell.copy().set_scope("n_stage_2").set_input([4, 5]))
+        self.add_node(7, subgraph=normal_cell.copy().set_scope("n_stage_2").set_input([5, 6]))
+        self.add_node(8, subgraph=reduction_cell.copy().set_scope("r_stage_2").set_input([6, 7]))
+        self.add_node(9, subgraph=normal_cell.copy().set_scope("n_stage_3").set_input([7, 8]))
+        self.add_node(10, subgraph=normal_cell.copy().set_scope("n_stage_3").set_input([8, 9]))
+        self.add_node(11)   # output
 
-        j = 3   # index of next node to add
-        for scope, c in zip(self.OPTIMIZER_SCOPE, channels):
-            
-            # reduction cell (beginning of each stage but first)
-            if j > 3:
-                input = [j-2, j-1]
-                edge_prev = (input[1], j)
-                edge_prev_prev = (input[0], j)
-                
-                self.add_node(j, subgraph=reduction.copy().set_scope(scope).set_input(input))
-                self.add_edges_from([edge_prev, edge_prev_prev])
+        self.add_edges_from([(i, i+1) for i in range(1, 11)])
+        self.add_edges_from([(i, i+2) for i in range(2, 9)])
+ 
+        #
+        # Operations at the makrograph edges
+        #
+        self.num_in_edges = 4
+        reduction_cell_indices = [5, 8]
 
-                # with preprocessing we fix the number of input channels for each cell
-                C_in = c_prev * num_intermediate_nodes
-                self.edges[edge_prev].set('op', ops.ReLUConvBN(C_in, C_out=c, kernel_size=1, affine=affine))
-                self.edges[edge_prev_prev].set('op', ops.ReLUConvBN(C_in, C_out=c, kernel_size=1, affine=affine))
+        channel_map_from, channel_map_to = channel_maps(reduction_cell_indices, max_index=11)
 
-                j += 1
+        self._set_makrograph_ops(channel_map_from, channel_map_to, reduction_cell_indices, max_index=11, affine=False)
 
-            # normal cells
-            for i in range(cells_per_stage):
-                # single (copied) input if first cell after stem
-                input = [j-1, j-1] if j == 3 else [j-2, j-1]
-                edge_prev = (input[1], j)
-                edge_prev_prev = (input[0], j)
+        self._set_cell_ops(reduction_cell_indices)
 
-                self.add_node(j, subgraph=normal.copy().set_scope(scope).set_input(input))
-                self.add_edges_from([edge_prev, edge_prev_prev])
 
-                # with preprocessing we fix the number of input channels for each cell
-                C_in = c * stem_multiplier if j == 3 else c * num_intermediate_nodes
-                self.edges[edge_prev].set('op', ops.ReLUConvBN(C_in, C_out=c, kernel_size=1, affine=affine))
-                if i==0 and j>3:
-                    # we have a connection that jumps over a reduction cell
-                    self.edges[edge_prev_prev].set('op', FactorizedReduce(C_in//2, C_out=c, affine=affine)) 
+    def _set_makrograph_ops(self, channel_map_from, channel_map_to, reduction_cell_indices, max_index, affine=True):
+        # pre-processing
+        # In darts there is a hardcoded multiplier of 3 for the output of the stem
+        stem_multiplier = 3
+        self.edges[1, 2].set('op', ops.Stem(self.channels[0] * stem_multiplier))
+
+        # edges connecting cells
+        for u, v, data in sorted(self.edges(data=True)):
+            if u > 1 and v < max_index:
+                C_in = self.channels[channel_map_from[u]]
+                C_out = self.channels[channel_map_to[v]]
+                if C_in == C_out:
+                    C_in = C_in * stem_multiplier if u == 2 else C_in * self.num_in_edges     # handle Stem
+                    if v in reduction_cell_indices:
+                        C_out *= 2
+                    data.set('op', ops.ReLUConvBN(C_in, C_out, kernel_size=1, affine=affine))
                 else:
-                    if j <= 4:
-                        C_in = c * stem_multiplier
-                    self.edges[edge_prev_prev].set('op', ops.ReLUConvBN(C_in, C_out=c, kernel_size=1, affine=affine))
-                    
-                j += 1
-            c_prev = c
+                    data.set('op', FactorizedReduce(C_in * self.num_in_edges, C_out, affine=affine))
         
-        if auxiliary:
-            # Auxiliary, taken from DARTS implementation
-            # hardcoded, assuming input size 8x8
-            self.add_node(j)
-            self.add_edge(j-1, j, op=ops.Sequential(
-                nn.ReLU(inplace=True),
-                nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
-                nn.Conv2d(channels[-1] * num_intermediate_nodes, 128, 1, bias=False),
-                nn.BatchNorm2d(128),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(128, 768, 2, bias=False),
-                nn.BatchNorm2d(768),
-                nn.ReLU(inplace=True),
-                nn.Flatten(),
-                nn.Linear(768, self.num_classes)
-            ))
-            j += 1
-            self.add_node(j)    # output    
-            self.add_edge(j-2, j, op=ops.Sequential(    # post-processing (pooling, classifier)
-                nn.AdaptiveAvgPool2d(1),
-                nn.Flatten(),
-                nn.Linear(channels[-1] * num_intermediate_nodes, self.num_classes),
-            ))
-        else:
-            self.add_node(j)    # output    
-            self.add_edge(j-1, j, op=ops.Sequential(    # post-processing (pooling, classifier)
-                nn.AdaptiveAvgPool2d(1),
-                nn.Flatten(),
-                nn.Linear(channels[-1] * num_intermediate_nodes, self.num_classes),
-            ))
+        # post-processing
+        _, _, data = sorted(self.edges(data=True))[-1]
+        data.set('op', ops.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Linear(self.channels[-1] * self.num_in_edges, self.num_classes))
+        )
+
+
+    def _set_cell_ops(self, reduction_cell_indices):
+        # normal cells
+        stages = ["n_stage_1", "n_stage_2", "n_stage_3"]
+
+        for scope, c in zip(stages, self.channels):
+            self.update_edges(
+                update_func=lambda edge: _set_ops(edge, c, stride=1),
+                scope=scope,
+                private_edge_data=True
+            )
+
+        # reduction cells
+        # stride=2 is only for some edges, that's why we have to do it this way
+        for n, c in zip(reduction_cell_indices, self.channels[1:]):
+            reduction_cell = self.nodes[n]['subgraph']
+            for u, v, data in reduction_cell.edges.data():
+                stride = 2 if u in (1, 2) else 1
+                if not data.is_final():
+                    edge = AttrDict(data=data)
+                    _set_ops(edge, c, stride)
+
+        #
+        # Combining operations
+        #
+        for _, cell in sorted(self.nodes('subgraph')):
+            if cell:
+                cell.nodes[7]['comb_op'] = channel_concat
 
 
     def prepare_discretization(self):
@@ -221,39 +201,72 @@ class DartsSearchSpace(Graph):
         and 3 normal cells at each stage.
         """
         # this is called after the optimizer has discretized the graph
-
-        normal, reduction = self._get_child_graphs(True)
-        self.clear()
-
-        # We need to reset the initialized ops because the channels will change
-        def reset_op(edge):
-            op_attr = edge.data.op.init_params
-            edge.data.set('op', edge.data.op.__class__)
-            edge.data.update(op_attr)
+        self._expand()
         
-        normal.update_edges(reset_op)
-        reduction.update_edges(reset_op)
+        # Operations at the edges
+        self.channels = [36, 72, 144]
+        reduction_cell_indices = [9, 16]
 
-        channels = [36, 72, 144]
+        channel_map_from, channel_map_to = channel_maps(reduction_cell_indices, max_index=23)
+        self._set_makrograph_ops(channel_map_from, channel_map_to, reduction_cell_indices, max_index=23, affine=True)
+
+        # Taken from DARTS implementation
+        # assuming input size 8x8
+        self.edges[22, 23].set('op', ops.Sequential(
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
+            nn.Conv2d(self.channels[-1] * self.num_in_edges, 128, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 768, 2, bias=False),
+            nn.BatchNorm2d(768),
+            nn.ReLU(inplace=True),
+            nn.Flatten(),
+            nn.Linear(768, self.num_classes))
+        )
+
+        self.update_edges(
+            update_func=_double_channels,
+            scope=self.OPTIMIZER_SCOPE,
+            private_edge_data=True
+        )
+
+
+    def _expand(self):
+        # shift the node indices to make space for 4 more nodes at each stage
+        # and the auxiliary logits
+        mapping = {
+            5: 9,
+            6: 10,
+            7: 11,
+            8: 16,
+            9: 17,
+            10: 18,
+            11: 24,     # 23 is auxiliary
+        }
+        nx.relabel_nodes(self, mapping, copy=False)
         
-        self.build_macrograph(channels, (normal, reduction), cells_per_stage=6, affine=True, auxiliary=True)
-
-        def set_channels(edge, C):
-            edge.data.set('C_in', C)
-            edge.data.set('C_out', C)
-            edge.data.set('affine', True)
-
-        for scope, c in zip(self.OPTIMIZER_SCOPE, channels):
-            self.update_edges(lambda edge: set_channels(edge, c), scope, private_edge_data=True)
-
-        self.compile()
+        # fix edges
+        self.remove_edges_from(list(self.edges()))
+        self.add_edges_from([(i, i+1) for i in range(1, 22)])
+        self.add_edges_from([(i, i+2) for i in range(2, 21)])
+        self.add_edge(22, 23)   # auxiliary output
+        self.add_edge(22, 24)   # final output
         
+        to_insert = [] + list(range(5, 9)) + list(range(12, 16)) + list(range(19, 23))
+        for i in to_insert:
+            normal_cell = self.nodes[i-1]['subgraph']
+            self.add_node(i, subgraph=normal_cell.copy().set_scope(normal_cell.scope).set_input([i-2, i-1]))
+        
+        for i, cell in sorted(self.nodes(data='subgraph')):
+            if cell:
+                if i == 3:
+                    cell.input_node_idxs = [2, 2]
+                else:
+                    cell.input_node_idxs = [i-2, i-1]
+
 
     def auxilary_logits(self):
-        """
-        Return the axiliary logits as a additional function which can or cannot be
-        used during evaluation.
-        """
         return self.graph['out_from_23']
 
 
@@ -291,6 +304,32 @@ class DartsSearchSpace(Graph):
         return "normal={} | reduction={}".format(convert(normal_cell), convert(reduction_cell))
 
 
+def _set_ops(edge, C, stride):
+    """
+    Replace the 'op' at the edges with the ones defined here.
+    This function is called by the framework for every edge in
+    the defined scope.
+    Args:
+        current_egde_data (EdgeData): The data that currently sits
+            at the edge.
+        C (int): convolutional channels
+        stride (int): stride for the operation
+    
+    Returns:
+        EdgeData: the updated EdgeData object.
+    """
+    edge.data.set('op', [
+        ops.Identity() if stride==1 else FactorizedReduce(C, C, stride, affine=False),
+        ops.Zero(stride=stride),
+        ops.MaxPool(3, stride),
+        ops.AvgPool(3, stride),
+        ops.SepConv(C, C, kernel_size=3, stride=stride, padding=1, affine=False),
+        ops.SepConv(C, C, kernel_size=5, stride=stride, padding=2, affine=False),
+        ops.DilConv(C, C, kernel_size=3, stride=stride, padding=2, dilation=2, affine=False),
+        ops.DilConv(C, C, kernel_size=5, stride=stride, padding=4, dilation=2, affine=False),
+    ])
+
+
 def _truncate_input_edges(node, in_edges, out_edges):
     """
     Removes input edges if there are more than k.
@@ -320,5 +359,34 @@ def _truncate_input_edges(node, in_edges, out_edges):
                     in_edges[random.randint(0, len(in_edges)-1)][1].delete()
 
 
+def _double_channels(edge):
+    init_params = edge.data.op.init_params
+    if 'C_in' in init_params:
+        init_params['C_in'] = int(init_params['C_in'] * 2.25) 
+    if 'C_out' in init_params:
+        init_params['C_out'] = int(init_params['C_out'] * 2.25) 
+    if 'affine' in init_params:
+        init_params['affine'] = True
+    edge.data.set('op', edge.data.op.__class__(**init_params))
+
+
 def channel_concat(tensors):
     return torch.cat(tensors, dim=1)
+
+
+def channel_maps(reduction_cell_indices, max_index):
+    # calculate the mapping from edge indices to the respective channel
+
+    assert len(reduction_cell_indices) == 2
+    r_1, r_2 = reduction_cell_indices
+    channel_map_from = {}
+    channel_map_from.update({i: 0 for i in range(2, r_1)})
+    channel_map_from.update({i: 1 for i in range(r_1, r_2)})
+    channel_map_from.update({i: 2 for i in range(r_2, max_index)})
+
+    channel_map_to = {}
+    channel_map_to.update({i: 0 for i in range(3, r_1+1)})
+    channel_map_to.update({i: 1 for i in range(r_1+1, r_2+1)})
+    channel_map_to.update({i: 2 for i in range(r_2+1, max_index)})
+
+    return channel_map_from, channel_map_to
