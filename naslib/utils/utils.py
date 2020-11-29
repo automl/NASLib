3634333c41f2a0771623afa2675d6744bfb98e2e
yangliu-re <yang@realityengines.ai>
2020-11-28 21:20:14 -0800

    Update utils.py
    implemented AverageMeterGroup and NamedAverageMeter classes

--- naslib/utils/utils.py
+++ naslib/utils/utils.py
@@ -316,6 +316,69 @@ class AttrDict(dict):
         self.__dict__ = self
 
 
+lass AverageMeterGroup:
+    """Average meter group for multiple average meters, ported from Naszilla repo."""
+
+    def __init__(self):
+        self.meters =  OrderedDict()
+
+    def update(self, data, n=1):
+        for k, v in data.items():
+            if k not in self.meters:
+                self.meters[k] = NamedAverageMeter(k, ":4f")
+            self.meters[k].update(v, n=n)
+
+    def __getattr__(self, item):
+        return self.meters[item]
+
+    def __getitem__(self, item):
+        return self.meters[item]
+
+    def __str__(self):
+        return "  ".join(str(v) for v in self.meters.values())
+
+    def summary(self):
+        return "  ".join(v.summary() for v in self.meters.values())
+
+
+class NamedAverageMeter:
+    """Computes and stores the average and current value, ported from naszilla repo"""
+
+    def __init__(self, name, fmt=':f'):
+        """
+        Initialization of AverageMeter
+        Parameters
+        ----------
+        name : str
+            Name to display.
+        fmt : str
+            Format string to print the values.
+        """
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+    def summary(self):
+        fmtstr = '{name}: {avg' + self.fmt + '}'
+        return fmtstr.format(**self.__dict__)
+
+
 class AverageMeter(object):
 
     def __init__(self):
