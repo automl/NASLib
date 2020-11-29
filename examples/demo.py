664c6fe1f72f6335840bc6af3b1083f9ee25db78
crwhite14 <colin@realityengines.ai>
2020-11-29 07:00:10 +0000

    fix examples/demo

--- examples/demo.py
+++ examples/demo.py
@@ -40,11 +40,12 @@ supported_optimizers = {
 # search_space = graph.NasBench101SearchSpace()
 # search_space = HierarchicalSearchSpace()
 # search_space = DartsSearchSpace()
-# search_space = NasBench201SearchSpace()
+search_space = NasBench201SearchSpace()
 
 # Changing the optimizer is one line of code
 
-optimizer = supported_optimizers['rs']
+optimizer = supported_optimizers[config.optimizer]
+optimizer.adapt_search_space(search_space)
 
 # Start the search and evaluation
 trainer = Trainer(optimizer, config)
