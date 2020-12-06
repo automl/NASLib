7ba3d8e4542b38a618c1add63b11f5c1fc7af28a
crwhite14 <colin@realityengines.ai>
2020-12-06 03:18:25 +0000

    set up batch predictor experiments

--- naslib/benchmarks/nasbench201/run_sequential.sh
+++ naslib/benchmarks/nasbench201/run_sequential.sh
@@ -1,5 +1,6 @@
 
-optimizers=(rs re ls bp bananas)
+#optimizers=(rs re ls bp bananas)
+optimizers=(rs re ls)
 epochs=150
 out_dir=run
 dataset=cifar10
@@ -20,7 +21,7 @@ for t in $(seq $start_seed $end_seed)
 do
     for opt in ${optimizers[@]}
     do
-        config_file=$out_dir/$dataset/configs/config\_$opt\_$t.yaml
+        config_file=$out_dir/$dataset/configs/nas/config\_$opt\_$t.yaml
         echo ================running $opt trial: $t epochs: $epochs=====================
         python naslib/benchmarks/nasbench201/runner.py --config-file $config_file
     done
