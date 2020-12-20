5080bdebb635d5711698b3dc58609537a88bd2ec
crwhite14 <colin@realityengines.ai>
2020-12-20 20:32:05 +0000

    add remote script

---
+++ naslib/benchmarks/predictors/run_remote.sh
@@ -0,0 +1,38 @@
+predictors=(lcsvr valloss valacc sotl bananas feedforward gbdt gcn bonas_gcn xgb ngb jacov)
+experiment_types=(vary_fidelity vary_fidelity vary_fidelity vary_fidelity vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size single)
+
+#experiment_types=(single single single single single single single single single single single single)
+
+#predictors=(feedforward)
+#experiment_types=(vary_train_size)
+
+out_dir=run
+search_space=nasbench201
+dataset=cifar10
+
+start_seed=$1
+trials=50
+
+end_seed=$(($start_seed + $trials - 1))
+test_size=200
+
+# create config files
+for i in $(seq 0 $((${#predictors[@]}-1)) )
+do
+    predictor=${predictors[$i]}
+    experiment_type=${experiment_types[$i]}
+    python NASLib/naslib/utils/create_configs.py --predictor $predictor --experiment_type $experiment_type \
+    --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
+    --dataset=$dataset --config_type predictor --search_space $search_space
+done
+
+# run experiments
+for t in $(seq $start_seed $end_seed)
+do
+ for predictor in ${predictors[@]}
+ do
+     config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
+     echo ================running $predictor trial: $t =====================
+     python NASLib/naslib/benchmarks/predictors/runner.py --config-file $config_file
+ done
+done
