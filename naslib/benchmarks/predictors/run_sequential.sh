3ce2647dc4e0cec41dc357096fa4a0b503ec25b5
crwhite14 <colin@realityengines.ai>
2021-01-10 04:25:03 +0000

    make out_dir unique

--- naslib/benchmarks/predictors/run_sequential.sh
+++ naslib/benchmarks/predictors/run_sequential.sh
@@ -12,25 +12,24 @@ vary_train_size vary_train_size vary_train_size vary_train_size \
 vary_train_size vary_train_size vary_train_size \
 vary_train_size vary_train_size vary_train_size)
 
-# folders:
-out_dir=p201_c10_jan9
-base_file=NASLib/naslib
-save_to_s3=true
-
-# search space / data:
-search_space=nasbench201
-dataset=cifar10
-
-# trials / seeds:
-trials=100
 start_seed=$1
-end_seed=$(($start_seed + $trials - 1))
 if [ -z "$start_seed" ]
 then
     start_seed=0
 fi
 
-# dataset sizes:
+# folders:
+out_dir=p201_c10_jan9_$start_seed
+base_file=NASLib/naslib
+
+# search space / data:
+search_space=nasbench201
+dataset=cifar10
+
+# other variables:
+trials=100
+end_seed=$(($start_seed + $trials - 1))
+save_to_s3=true
 test_size=200
 
 # create config files
