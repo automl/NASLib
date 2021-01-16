c37b87b5273685a6978fc7451877267d03036e17
crwhite14 <colin@realityengines.ai>
2021-01-16 20:39:20 +0000

    update run_darts

--- naslib/benchmarks/predictors/run_darts.sh
+++ naslib/benchmarks/predictors/run_darts.sh
@@ -1,47 +1,42 @@
-predictors=(valloss valacc sotl bananas \
-feedforward gbdt gcn bonas xgb \
-ngb rf jacov dngo bohamiann \
-bayes_lin_reg lcsvr gp sparse_gp \
-var_sparse_gp seminas) # lcnet
+predictors=(sotl valacc jacov snip \
+bananas feedforward bonas \
+gbdt xgb ngb rf \
+dngo bohamiann bayes_lin_reg \
+gp sparse_gp var_sparse_gp)
 
-experiment_types=(vary_fidelity vary_fidelity vary_fidelity vary_train_size \
-vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size \
-vary_train_size vary_train_size single vary_train_size vary_train_size \
-vary_train_size vary_train_size vary_fidelity vary_train_size \
-vary_train_size vary_train_size) # vary_train_size
+experiment_types=(vary_fidelity vary_fidelity single single \
+vary_train_size vary_train_size vary_train_size \
+vary_train_size vary_train_size vary_train_size vary_train_size \
+vary_train_size vary_train_size vary_train_size \
+vary_train_size vary_train_size vary_train_size)
 
-predictors=(sotl rf gp sparse_gp var_sparse_gp seminas)
-experiment_types=(vary_fidelity vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size)
-
-predictors=(sotl rf gp sparse_gp var_sparse_gp ngb)
-experiment_types=(vary_fidelity vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size)
-
-# folders:
-out_dir=run
-base_file=NASLib/naslib
-
-# search space / data:
-search_space=darts
-dataset=cifar10
-
-# trials / seeds:
-trials=100
 start_seed=$1
-end_seed=$(($start_seed + $trials - 1))
 if [ -z "$start_seed" ]
 then
     start_seed=0
 fi
 
-# dataset sizes:
-test_size=200
+# folders:
+base_file=NASLib/naslib
+s3_folder=p301_jan16
+out_dir=$s3_folder\_$start_seed
+
+# search space / data:
+search_space=darts
+dataset=cifar10
+
+# other variables:
+trials=100
+end_seed=$(($start_seed + $trials - 1))
+save_to_s3=true
+test_size=100
 
 # create config files
 for i in $(seq 0 $((${#predictors[@]}-1)) )
 do
     predictor=${predictors[$i]}
     experiment_type=${experiment_types[$i]}
-    python $base_file/utils/create_configs.py --predictor $predictor --experiment_type $experiment_type \
+    python $base_file/benchmarks/create_configs.py --predictor $predictor --experiment_type $experiment_type \
     --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
     --dataset=$dataset --config_type predictor --search_space $search_space
 done
@@ -49,10 +44,17 @@ done
 # run experiments
 for t in $(seq $start_seed $end_seed)
 do
- for predictor in ${predictors[@]}
- do
-     config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
-     echo ================running $predictor trial: $t =====================
-     python $base_file/benchmarks/predictors/runner.py --config-file $config_file
- done
+    for predictor in ${predictors[@]}
+    do
+        config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
+        echo ================running $predictor trial: $t =====================
+        python $base_file/benchmarks/predictors/runner.py --config-file $config_file
+    done
+    if [ "$save_to_s3" ]
+    then
+        # zip and save to s3
+        echo zipping and saving to s3
+        zip -r $out_dir.zip $out_dir 
+        python $base_file/benchmarks/upload_to_s3.py --out_dir $out_dir --s3_folder $s3_folder
+    fi
 done
