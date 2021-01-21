7f966ff7cbe9fed8ffcb6af051a026aaff238885
crwhite14 <colin@realityengines.ai>
2021-01-21 02:47:23 +0000

    fix merge error

--- naslib/benchmarks/predictors/run_nb201.sh
+++ naslib/benchmarks/predictors/run_nb201.sh
@@ -1,5 +1,5 @@
-predictors=(sotlema)
-experiment_types=(vary_fidelity)
+predictors=(omni_lofi)
+experiment_types=(vary_train_size)
 
 start_seed=$1
 if [ -z "$start_seed" ]
@@ -9,7 +9,7 @@ fi
 
 # folders:
 base_file=NASLib/naslib
-s3_folder=p201_c10_jan20
+s3_folder=p201_c10_jan20_2
 out_dir=$s3_folder\_$start_seed
 
 # search space / data:
