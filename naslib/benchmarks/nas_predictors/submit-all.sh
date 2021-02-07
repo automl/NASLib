0f3d97baabebcf4c13bd89a3f5b72ed7bf97b427
Arber Zela <zelaa@cs.uni-freiburg.de>
2021-02-07 17:04:09 +0100

    added minimal implementation of Deep GPs

--- naslib/benchmarks/nas_predictors/submit-all.sh
+++ naslib/benchmarks/nas_predictors/submit-all.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 #nb101
-predictors101=(bananas feedforward gbdt gcn xgb ngb rf dngo \
-	bohamiann bayes_lin_reg seminas nao gp sparse_gp var_sparse_gp)
+#predictors101=(bananas feedforward gbdt gcn xgb ngb rf dngo \
+	#bohamiann bayes_lin_reg seminas nao gp sparse_gp var_sparse_gp)
 
 #nb201
-predictors201=(bananas feedforward gbdt gcn bonas xgb ngb rf dngo \
- 	bohamiann bayes_lin_reg seminas nao gp sparse_gp var_sparse_gp)
+#predictors201=(bananas feedforward gbdt gcn bonas xgb ngb rf dngo \
+ 	#bohamiann bayes_lin_reg seminas nao gp sparse_gp var_sparse_gp)
+predictors201=(ngb_hp omni)
 
 #nb301
 #predictors301=(bananas feedforward gbdt bonas xgb ngb rf dngo \
 # 	bohamiann bayes_lin_reg gp sparse_gp var_sparse_gp nao)
 
-for predictor in ${predictors101[@]}
-do
-    sbatch -J 101-${predictor} slurm_job-nb101.sh $predictor
-done
+#for predictor in ${predictors101[@]}
+#do
+    #sbatch -J 101-${predictor} slurm_job-nb101.sh $predictor
+#done
 
 for predictor in ${predictors201[@]}
 do
     #sbatch -J 201-${predictor} slurm_job-nb201-c10.sh $predictor
-    sbatch -J c100-201-${predictor} slurm_job-nb201-c100.sh $predictor
-    sbatch -J imnet-201-${predictor} slurm_job-nb201-imagenet.sh $predictor
+    #sbatch -J c100-201-${predictor} slurm_job-nb201-c100.sh $predictor
+    #sbatch -J imnet-201-${predictor} slurm_job-nb201-imagenet.sh $predictor
+    sbatch -J imnet-201-${predictor} slurm_job-imgnet.sh $predictor
 done
 
 #for predictor in ${predictors301[@]}
