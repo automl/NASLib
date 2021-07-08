9ab2c647de0a8dc93602dd7616a80cfca8623023
yashsmehta <yashsmehta95@gmail.com>
2021-07-08 18:04:08 +0200

    🎨 code cleanup

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import sys
 import subprocess
 from setuptools import setup, find_packages
 
-VERBOSE_SCRIPT = True
-
 # Check for python version
 if sys.version_info.major != 3 or sys.version_info.minor < 6 or sys.version_info.minor > 7:
     raise ValueError(
@@ -18,9 +16,6 @@ cwd = os.path.dirname(os.path.abspath(__file__))
 version_path = os.path.join(cwd, 'naslib', '__version__.py')
 with open(version_path) as fh:
     version = fh.readlines()[-1].split()[-1].strip("\"'")
-
-if VERBOSE_SCRIPT:
-    print('-- Building version ' + version)
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -43,22 +38,24 @@ except ImportError:
             git_nasbench], check=False)
 
 
-if __name__=='__main__':
-    setup(
-        name='naslib',
-        version=version,
-        description='NASLib: A modular and extensible Neural Architecture Search (NAS) library.',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        author='AutoML Freiburg',
-        author_email='zelaa@cs.uni-freiburg.de',
-        url='https://github.com/automl/NASLib',
-        license='Apache License 2.0',
-        classifiers=['Development Status :: 1 - Beta'],
-        packages=find_packages(),
-        python_requires='>=3.6',
-        platforms=['Linux'],
-        install_requires=requirements,
-        keywords=['NAS', 'automl'],
-        test_suite='pytest'
-    )
+print('-- Building version ' + version)
+print('-- Note: by default installs pytorch-cpu version (1.9.0), update to torch-gpu by following instructions from: https://pytorch.org/get-started/locally/')
+
+setup(
+    name='naslib',
+    version=version,
+    description='NASLib: A modular and extensible Neural Architecture Search (NAS) library.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='AutoML Freiburg',
+    author_email='zelaa@cs.uni-freiburg.de',
+    url='https://github.com/automl/NASLib',
+    license='Apache License 2.0',
+    classifiers=['Development Status :: 1 - Beta'],
+    packages=find_packages(),
+    python_requires='>=3.6',
+    platforms=['Linux'],
+    install_requires=requirements,
+    keywords=['NAS', 'automl'],
+    test_suite='pytest'
+)
