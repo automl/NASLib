37d2f72e6f2658498ddc0badfe07489af9d849ba
Arjun <arjun.krishnakumar.mec@gmail.com>
2021-10-04 17:44:08 +0200

    Fix bug in transbench101 conversion

--- naslib/search_spaces/transbench101/conversions.py
+++ naslib/search_spaces/transbench101/conversions.py
@@ -13,6 +13,9 @@ naslib -> arch_str
 Note: we could add more conversions, but this is all we need for now
 """
 
+from naslib.search_spaces.core.graph import Graph
+
+
 OP_NAMES = ['Identity', 'Zero', 'ReLUConvBN3x3', 'ReLUConvBN1x1']
 EDGE_LIST = ((1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4))
 
@@ -90,7 +93,8 @@ def convert_naslib_to_str(naslib_object):
         'Zero': 'none',
     }
 
-    cell = naslib_object.edges[2, 3].op
+    cell = naslib_object.nodes[2]['subgraph'].edges[1, 2]['op'].op[1] # TODO: Do this in a clean fashion
+    assert cell.name == "cell" and isinstance(cell, Graph)
     
     edge_op_dict = {
         (i, j): ops_to_nb201[cell.edges[i, j]['op'].get_op_name] for i, j in cell.edges
@@ -115,7 +119,8 @@ def convert_naslib_to_transbench101_micro(naslib_object):
         'Zero': '0',
     }
 
-    cell = naslib_object.edges[2, 3].op
+    cell = naslib_object.nodes[2]['subgraph'].edges[1, 2]['op'].op[1]
+    assert cell.name == "cell" and isinstance(cell, Graph)
     
     edge_op_dict = {
         (i, j): ops_to_tb101[cell.edges[i, j]['op'].get_op_name] for i, j in cell.edges
