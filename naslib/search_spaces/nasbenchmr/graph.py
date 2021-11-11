2ab518b675f2f8093e4d0df905c569dfe0a6a9d4
crwhite14 <colin@realityengines.ai>
2021-11-11 04:39:29 +0000

    updates to statistics

--- naslib/search_spaces/nasbenchmr/graph.py
+++ naslib/search_spaces/nasbenchmr/graph.py
@@ -11,30 +11,68 @@ from naslib.search_spaces.nasbenchmr.conversions import (
     convert_naslib_to_op_indices,
 )
 
+
+
+
+# this is a hack currently used to run statistics. 
+FIXED_DATA = None
+# 'video', '3ddet', 'seg', 'cls-50-1000'
+
 class NasBenchMRSearchSpace(Graph):
     """
     Contains the interface to the tabular benchmark of MR
     """
 
     QUERYABLE = True
+    
 
     def __init__(self):
         super().__init__()
         self.op_indices = [] # not sure about this line
-
+        self.load_labeled = None
+        self.index = None
         self.space_name = "nasbench_MR"
+
+    def load_labeled_architecture(self, dataset_api=None, dataset=FIXED_DATA):
+        """
+        Currently this method needs access to the dataset.
+        Best thing would be to remove that dependence.
+        """
+        index = np.random.choice(len(dataset_api[dataset]))
+        embedding = dataset_api[dataset][index]["embedding"]
+        op_indices = convert_params_to_op_indices(embedding=embedding)
+        self.set_op_indices(op_indices)
+        self.load_labeled = True
+        self.set_index(index)
+        
+    def set_spec(self, index, dataset_api=None, dataset=FIXED_DATA):
+        """
+        Currently this method needs access to the dataset.
+        Best thing would be to remove that dependence.
+        """
+        embedding = dataset_api[dataset][index]["embedding"]
+        op_indices = convert_params_to_op_indices(embedding=embedding)
+        self.set_op_indices(op_indices)
+        self.load_labeled = True
+        self.set_index(index)
+        
+    def get_arch_iterator(self, dataset_api=None, dataset=FIXED_DATA):
+        """
+        Currently this method needs access to the dataset.
+        Best thing would be to remove that dependence.
+        """
+        return range(len(dataset_api[dataset]))
 
     def query(
         self, metric=None,
-        dataset=None,
+        dataset=FIXED_DATA,
         path=None,
         epoch=-1,
         full_lc=False,
         dataset_api=None,
     ):
-
         """
-        Query results from ncp
+        Query results from ncp.
         """
         start = time.time()
         if metric != Metric.VAL_ACCURACY:
@@ -44,11 +82,26 @@ class NasBenchMRSearchSpace(Graph):
             return -1
 
         embedding = convert_op_idices_to_params(self.op_indices)
-
-        naslib_state = np.random.get_state()
-        query_result = dataset_api["api"].query(task=dataset, data_embedding=embedding)
-        np.random.set_state(naslib_state)
-        return query_result.get('main_metric')
+        params = convert_op_idices_to_params(self.op_indices)
+        if self.load_labeled:
+            """
+            If we loaded the architecture from the nasbenchmr training data (using
+            load_labeled_architecture()), then self.compact will contain the architecture spec,
+            and we can query the final train loss from the training data used to train the surrogates 
+            """
+            embedding_idx = self.get_index()
+            query_result = dataset_api[dataset][embedding_idx]
+            if dataset == '3ddet':
+                result = query_result['moderate']
+            elif 'cls' in dataset or 'video' in dataset:
+                result = query_result['top1']
+            elif 'seg' in dataset:
+                result = query_result['mIoU']
+        else:
+            query_result = dataset_api["api"].query(task=dataset, data_embedding=embedding)
+            print(time.time()-start)
+            result = query_result.get('main_metric')
+        return result
 
     def get_op_indices(self):
         if self.op_indices is None:
@@ -60,30 +113,43 @@ class NasBenchMRSearchSpace(Graph):
         self.op_indices = op_indices
         # convert_op_indices_to_naslib(self)
 
+    def set_index(self, index):
+        self.index = index
+
+    def get_index(self):
+        return self.index
+
     def get_hash(self):
         return tuple(self.get_op_indices())
 
     def sample_random_architecture(self, dataset_api=None):
         """
-        This will sample a random architecture and update the edges in the
-        naslib object accordingly.
+        This will sample a random architecture.
+        Currently a hack is used to ensure randomness across trials.
         """
+        st0 = np.random.get_state()
+        np.random.seed(int(time.time()))
         op_indices = np.random.randint(2, size=80)
         self.set_op_indices(op_indices)
+        np.random.set_state(st0)
 
     def mutate(self, parent, dataset_api=None):
         """
         This will mutate one op from the parent op indices, and then
-        update the naslib object and op_indices
+        update the naslib object and op_indices.
+        Currently a hack is used to ensure randomness across trials.
         """
 
         parent_op_indices = parent.get_op_indices()
         op_indices = parent_op_indices
 
+        st0 = np.random.get_state()
+        np.random.seed(int(time.time()))
         edge = np.random.choice(len(parent_op_indices))
         # flips the current op on the edge
         op_indices[edge] = 1-op_indices[edge]
         self.set_op_indices(op_indices)
+        np.random.set_state(st0)
 
     def find_nbr_params(self, param):
         if param <= 4:
@@ -105,7 +171,10 @@ class NasBenchMRSearchSpace(Graph):
 
     def get_nbhd(self, dataset_api=None):
         # returns all neighbors of the architecture
+        #Currently a hack is used to ensure randomness across trials.
         # arch_embedding = convert_naslib_graph_to_params(self)
+        st0 = np.random.get_state()
+        np.random.seed(int(time.time()))
         nbr_embeddings = []
         arch_embedding = convert_op_idices_to_params(self.op_indices)
         for param_idx in range(len(arch_embedding)):
@@ -122,8 +191,8 @@ class NasBenchMRSearchSpace(Graph):
 
 
         random.shuffle(nbr_embeddings)
+        np.random.set_state(st0)
         return nbr_embeddings
 
     def get_type(self):
         return "nasbench_MR"
-
--- naslib/utils/get_dataset_api.py
+++ naslib/utils/get_dataset_api.py
@@ -122,7 +122,16 @@ def get_mr_api(dataset=None):
     Load the NAS-Bench-MR
     """
     from ncp.tools import api
-    return {'api': api}
+    with open(
+            os.path.join(
+                "/home/ubuntu/NCP/ncp/data/", dataset, "trainval.pkl"
+            ),
+            "rb",
+    ) as f:
+        data = pickle.load(f)
+
+    return {'api': api,
+            dataset: data}
 
 
 def get_asr_api(dataset=None):
