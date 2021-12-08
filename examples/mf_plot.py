71d9dbf9a2bd7f1113d4bf24efa847cdef89400c
Lars Gerne <lage2104@gmail.com>
2021-12-08 20:09:00 +0100

    delete obsolete function from mf plots

--- examples/mf_plot.py
+++ examples/mf_plot.py
@@ -4,31 +4,6 @@ import matplotlib.pyplot as plt
 
 from collections import defaultdict
 
-def restructure_results(errors :dict):
-    """This super fancy function restructures the results in order plot successive halving correctly
-    NOTE: This in not optimimzed at all!
-    NOTE: this function should be obsolete thus we save the stats now in the correct format.
-    """
-    print(errors[1].keys())
-    len_errors = len(errors[1]['train_acc'])
-    train_acc = errors[1]['train_acc']
-    valid_acc = errors[1]['valid_acc']
-    fidelity = errors[1]['fidelity']
-    archs = errors[1]['arch']
-    test_acc = errors[1]['test_acc']
-    runtime = errors[1]['runtime']
-    train_time = errors[1]['train_time']
-    new_errors = defaultdict(lambda: defaultdict(list))
-
-    for idx in range(len_errors):
-        new_errors[archs[idx]]['train_acc'].append(train_acc[idx])
-        new_errors[archs[idx]]['valid_acc'].append(valid_acc[idx])
-        new_errors[archs[idx]]['fidelity'].append(fidelity[idx])
-        new_errors[archs[idx]]['test_acc'].append(test_acc[idx])
-        new_errors[archs[idx]]['runtime'].append(runtime[idx])
-        new_errors[archs[idx]]['train_time'].append(train_time[idx])
-    return new_errors
-
 def get_results(predictor, path, epochs, metric='valid_acc', dataset='cifar10', ug=False):
     """
     Get statistics for successive halving
