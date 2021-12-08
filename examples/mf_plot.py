2e3d6ed40652eca044aa3fbeda36bd47a23c3489
Lars Gerne <lage2104@gmail.com>
2021-12-08 20:36:35 +0100

    add hb plot function

--- examples/mf_plot.py
+++ examples/mf_plot.py
@@ -1,37 +1,55 @@
 import os
 import json
+from matplotlib.lines import _LineStyle
 import matplotlib.pyplot as plt
 
 from collections import defaultdict
 
-def get_results(predictor, path, epochs, metric='valid_acc', dataset='cifar10', ug=False):
+def get_results(predictor, path, filename, metric='valid_acc'):
     """
     Get statistics for successive halving
-    # TODO: make metric selectable, currently 'val_acc' is fixed 
+    TODO: make metric selectable, currently 'val_acc' is fixed 
     """
     algo_path = os.path.join(path, predictor)
     for seed_dir in os.listdir(algo_path):
-        result_file = os.path.join(algo_path, seed_dir, 'sh_stats.json')
+        result_file = os.path.join(algo_path, seed_dir, filename)
         result = json.load(open(result_file))
         return result
 
 def plot_sh():
-    """Plots successive halving learning curves
     """
-    # set up parameters for the experiments
-    epochs = 300
-
+    Plots learning curves for successive halving
+    """
     folder = os.path.expanduser('./run/cifar10/nas_predictors/nasbench201')
     predictor = 'var_sparse_gp'
-    results = get_results(predictor, folder, epochs=epochs, metric='test_acc', ug=True)
+    results = get_results(predictor, folder, 'sh_stats.json', metric='test_acc')
 
-    for arch, results in results.items():
-        x = results['fidelity']
-        values = results['val_acc']
+    for arch, stats in results.items():
+        x = stats['fidelity']
+        values = stats['val_acc']
         plt.plot(x, values, linestyle='-', label=arch)
     plt.rcParams['grid.linestyle'] = 'dotted'
     plt.show()
     plt.savefig('plot_nb201.pdf', bbox_inches = 'tight', pad_inches = 0.1)
+
+def plot_hb():
+    """
+    Plots learning curves for hyperband
+    """
+    folder = os.path.expanduser('./run/cifar10/nas_predictors/nasbench201')
+    predictor = 'var_sparse_gp'
+    results = get_results(predictor, folder, 'hb_stats.json', metric='test_acc')
+    s = len(results)
+    
+    figure, axis = plt.subplots(s, 1)
+    for sh, sh_stats in results.items():
+        for arch, stats in sh_stats.items():
+            x = stats['fidelity']
+            values = stats['val_acc']
+            axis[sh, 0].plot(x, values, linestyle='-', label=arch)
+    plt.show()
+
+
 
 if __name__ == '__main__':
     plot_sh()
