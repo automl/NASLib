7175e0e50a936082d46658060d9d4bb38c1febd9
Arjun <arjun.krishnakumar.mec@gmail.com>
2022-02-21 19:05:00 +0100

    Improve test_benchmark_apis.py

--- test_benchmark_apis.py
+++ test_benchmark_apis.py
@@ -3,15 +3,59 @@ import os
 import argparse
 import contextlib
 
-from naslib.search_spaces import NasBench101SearchSpace, NasBench201SearchSpace, DartsSearchSpace, NasBenchNLPSearchSpace, NasBenchASRSearchSpace
+from naslib.search_spaces import (
+        NasBench101SearchSpace,
+        NasBench201SearchSpace,
+        DartsSearchSpace,
+        NasBenchNLPSearchSpace,
+        NasBenchASRSearchSpace,
+        TransBench101SearchSpaceMacro,
+        TransBench101SearchSpaceMicro
+    )
 from naslib.search_spaces.core.query_metrics import Metric
 from naslib.utils import get_dataset_api
 
+search_spaces = {
+    'nasbench101': NasBench101SearchSpace,
+    'nasbench201': NasBench201SearchSpace,
+    'darts': DartsSearchSpace,
+    'nlp': NasBenchNLPSearchSpace,
+    'asr': NasBenchASRSearchSpace,
+    'transbench101_micro': TransBench101SearchSpaceMicro,
+    'transbench101_macro': TransBench101SearchSpaceMacro,
+}
+
+tasks = {
+    'nasbench101': ['cifar10'],
+    'nasbench201': ['cifar10', 'cifar100', 'ImageNet16-120'],
+    'darts': ['cifar10'],
+    'nlp': ['treebank'],
+    'asr': ['timit'],
+    'transbench101_micro': [
+        'class_scene',
+        'class_object',
+        'jigsaw',
+        'room_layout',
+        'segmentsemantic',
+        'normal',
+        'autoencoder'
+    ],
+    'transbench101_macro': [
+        'class_scene',
+        'class_object',
+        'jigsaw',
+        'room_layout',
+        'segmentsemantic',
+        'normal',
+        'autoencoder'
+    ]
+}
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--search_space', required=False, type=str)
-parser.add_argument('--dataset', required=False, type=str)
-parser.add_argument('--all', required=False, action='store_true', help='Test all the benchmark APIs. Overrides --search_space and --dataset.')
+parser.add_argument('--search_space', required=False, type=str, help=f'API to test. Options: {list(search_spaces.keys())}')
+parser.add_argument('--task', required=False, type=str)
+parser.add_argument('--all', required=False, action='store_true', help='Test all the benchmark APIs. Overrides --search_space and --task.')
 parser.add_argument('--show_error', required=False, action='store_true', help='Show the exception raised by the APIs if they crash.')
 args = parser.parse_args()
 
@@ -37,49 +81,35 @@ def test_api(graph, search_space, dataset, metric):
     return result
 
 if __name__ == '__main__':
-    search_spaces = {
-        'nasbench101': NasBench101SearchSpace,
-        'nasbench201': NasBench201SearchSpace,
-        'darts': DartsSearchSpace,
-        'nlp': NasBenchNLPSearchSpace,
-        'asr': NasBenchASRSearchSpace
-    }
-
-    datasets = {
-        'nasbench101': ['cifar10'],
-        'nasbench201': ['cifar10', 'cifar100', 'ImageNet16-120'],
-        'darts': ['cifar10'],
-        'nlp': ['treebank'],
-        'asr': ['timit']
-    }
-
     if args.all == True:
         success = []
         fail = []
         for space in search_spaces.keys():
-            for dataset in datasets[space]:
+            for task in tasks[space]:
                 try:
-                    print(f'Testing api for {space} search space with {dataset}...', end=" ", flush=True)
+                    print(f'Testing (search_space, task) api for ({space}, {task}) ...', end=" ", flush=True)
                     with nullify_all_output():
                         graph = search_spaces[space]()
-                        result = test_api(graph, space, dataset, Metric.VAL_ACCURACY)
+                        result = test_api(graph, space, task, Metric.VAL_ACCURACY)
                     print('Success')
                 except Exception as e:
                     print('Fail')
                     if args.show_error:
                         print(e)
     else:
-        assert args.search_space is not None and args.dataset is not None, "Search space and dataset must be specified."
-        try:
-            print(f'Testing api for {args.search_space} with {args.dataset}...', end=" ", flush=True)
-            with nullify_all_output():
-                graph = search_spaces[args.search_space]()
-                result = test_api(graph, args.search_space, args.dataset, Metric.VAL_ACCURACY)
-            print('Success')
-            print(result)
-        except Exception as e:
-            print('Fail')
-            if args.show_error:
-                print(e)
+        assert args.search_space is not None, "Search space must be specified."
+        search_space_tasks = tasks[args.search_space] if args.task is None else [args.task]
+
+        for task in search_space_tasks:
+            try:
+                print(f'Testing (search_space, task) api for ({args.search_space}, {task})...', end=" ", flush=True)
+                with nullify_all_output():
+                    graph = search_spaces[args.search_space]()
+                    result = test_api(graph, args.search_space, task, Metric.VAL_ACCURACY)
+                print('Success')
+            except Exception as e:
+                print('Fail')
+                if args.show_error:
+                    print(e)
 
 
