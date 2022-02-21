16604a681a6617daa8774ef4acff656f36066f44
Lars Gerne <lage2104@gmail.com>
2022-02-21 12:01:50 +0100

    improvements in sbatch submit for mf experiments

--- naslib/benchmarks/mf/submit_configs.sh
+++ naslib/benchmarks/mf/submit_configs.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# define variable names
+workspace=/work/dlclarge2/gernel-naslib-experiments/
+
+
 #SBATCH -p mlhiwidlc_gpu-rtx2080 #bosch_gpu-rtx2080    #bosch_cpu-cascadelake  # partition (queue)
 #SBATCH -t 0-15:00           # time (D-HH:MM)
-#SBATCH -o log/%A.%N.out       # STDOUT  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
-#SBATCH -e log/%A.%N.err       # STDERR  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
+#SBATCH -o $workspace/log/%A.%N.out       # STDOUT  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
+#SBATCH -e $workspace/log/%A.%N.err       # STDERR  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
 #SBATCH -J naslib-bbo-rs            # sets the job name. 
 #SBATCH --mem=7G  
-#SBATCH --chdir=/work/dlclarge2/gernel-naslib-experiments/NASLib
+#SBATCH --chdir=$workspace/NASLib
 # Print some information about the job to STDOUT
 
 echo "Workingdir: $PWD";
 echo "Started at $(date)";
 echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
 
-config_dir=/work/dlclarge2/gernel-naslib-experiments/NASLib/naslib/benchmarks/mf/configs/nasbench201/cifar10/rs
+config_dir=$workspace/NASLib/naslib/benchmarks/mf/configs/nasbench201/cifar10/rs
 
 
 # python -u runner.py --config-file $1
@@ -21,7 +25,7 @@ for config in $config_dir/*
     for config_file_seed in $config/*
         do
             echo submitted ${config_file_seed}
-            python -u /work/dlclarge2/gernel-naslib-experiments/NASLib/naslib/benchmarks/bbo/runner.py --config-file $config_file_seed
+            /home/gernel/miniconda3/bin/python -u $workspace/NASLib/naslib/benchmarks/bbo/runner.py --config-file $config_file_seed
         done
     done
  
