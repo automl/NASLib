6c5289ab94c6c9fa27b1972bd9aa64dd4539fbd8
gernel <gernel@informatik.uni-freiburg.de>
2022-03-01 23:16:05 +0100

    fix sbatch

--- naslib/benchmarks/mf/submit_configs.sh
+++ naslib/benchmarks/mf/submit_configs.sh
@@ -4,11 +4,12 @@ workspace=/work/dlclarge2/gernel-naslib-experiments
 
 
 #SBATCH -p mlhiwidlc_gpu-rtx2080 #bosch_gpu-rtx2080    #bosch_cpu-cascadelake  # partition (queue)
-#SBATCH -t 0-15:00           # time (D-HH:MM)
+#SBATCH -t 0-1:00           # time (D-HH:MM)
 #SBATCH -o /work/dlclarge2/gernel-naslib-experiments/log/%A.%N.out       # STDOUT  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
 #SBATCH -e /work/dlclarge2/gernel-naslib-experiments/log/%A.%N.err       # STDERR  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
 #SBATCH -J naslib-bbo-rs            # sets the job name. 
-#SBATCH --mem=7G
+#SBATCH --mem=40G
+#SBATCH -c=8
 #SBATCH --chdir=/work/dlclarge2/gernel-naslib-experiments/NASLib
 # Print some information about the job to STDOUT
 
@@ -16,13 +17,13 @@ echo "Workingdir: $PWD";
 echo "Started at $(date)";
 echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
 
-config_dir=$workspace/NASLib/naslib/benchmarks/mf/configs/nasbench201/cifar10/rs
+config_dir=$workspace/NASLib/naslib/benchmarks/mf/configs/nasbench201/cifar10/sh
 if [[ $# -eq 0 ]] ; then
     echo 'some message'
     exit 0
 fi
-start=${args[0]} 
-end=${args[1]}
+start=$1
+end=$2
 echo "Starting at config $start"
 # python -u runner.py --config-file $1
 i=-1
@@ -31,12 +32,11 @@ for config in $config_dir/*
     for config_file_seed in $config/*
         do
             ((i=i+1))
-            echo $i
-            if [[ $i -lt $start ]] && [[$i -ge $end]]; then # &&  $i -gt $end ]] ; then
-                continue
-            fi
-            echo submitted ${config_file_seed}
-            echo "/home/gernel/miniconda3/bin/python -u $workspace/NASLib/naslib/benchmarks/mf/runner.py --config-file $config_file_seed"
+            if [[ "$i" -ge "$start" ]] && [[ "$i" -lt "$end" ]]; then # &&  $i -gt $end ]] ; then
+		echo $i
+            	echo submitted ${config_file_seed}
+		echo "/home/gernel/miniconda3/bin/python -u $workspace/NASLib/naslib/benchmarks/mf/runner.py --config-file $config_file_seed"
+	    fi
         done
     done
  
