a1800075026bfd5ecff6eff4bf36e9d6d502df7a
Arber Zela <zelaa@cs.uni-freiburg.de>
2022-03-09 19:43:12 +0100

    removed NAS optimizers and unrequired predictors

--- naslib/predictors/__init__.py
+++ naslib/predictors/__init__.py
@@ -1,19 +1,3 @@
 from .predictor import Predictor
-from .bonas import BonasPredictor
-from .bnn import BayesianLinearRegression, BOHAMIANN, DNGOPredictor
-from .early_stopping import EarlyStopping
-from .ensemble import Ensemble
-from .gcn import GCNPredictor
-from .gp import GPPredictor, SparseGPPredictor, VarSparseGPPredictor, GPWLPredictor
-from .lce import LCEPredictor
-from .lce_m import LCEMPredictor
-from .lcsvr import SVR_Estimator
-from .mlp import MLPPredictor
-from .oneshot import OneShotPredictor
-from .seminas import SemiNASPredictor
-from .soloss import SoLosspredictor
-from .trees import LGBoost, NGBoost, RandomForestPredictor, XGBoost
 from .zerocost_v1 import ZeroCostV1
 from .zerocost_v2 import ZeroCostV2
-from .omni_ngb import OmniNGBPredictor
-from .omni_seminas import OmniSemiNASPredictor
--- naslib/predictors/oneshot/__init__.py
+++
@@ -1,1 +0,0 @@
-from .oneshot import OneShotPredictor
