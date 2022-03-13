a3039384933b461dc86333ae2b7ab513754e9d77
Arjun <arjun.krishnakumar@students.uni-freiburg.de>
2022-03-13 16:47:54 +0100

    Add utility function to get search space from name and dataset
    - naslib.search_space.get_search_space(name, dataset) returns an initialized
      search space graph based on the name and dataset
    - Update runner.py to consume new function

--- naslib/search_spaces/__init__.py
+++ naslib/search_spaces/__init__.py
@@ -8,3 +8,32 @@ from .transbench101.graph import TransBench101SearchSpaceMicro
 from .transbench101.graph import TransBench101SearchSpaceMacro
 
 from .transbench101.api import TransNASBenchAPI
+
+
+supported_search_spaces = {
+    "darts": DartsSearchSpace,
+    "nasbench101": NasBench101SearchSpace,
+    "nasbench201": NasBench201SearchSpace,
+    "nlp": NasBenchNLPSearchSpace,
+    "asr": NasBenchASRSearchSpace,
+    "nastbenchsize": NATSBenchSizeSearchSpace,
+    'transbench101_micro': TransBench101SearchSpaceMicro,
+    'transbench101_macro': TransBench101SearchSpaceMacro,
+}
+
+dataset_n_classes = {
+    "cifar10": 10,
+    "cifar100": 100,
+    "imagenet16-120": 120,
+}
+
+def get_search_space(name, dataset):
+    search_space_cls = supported_search_spaces[name.lower()]
+    n_classes = dataset_n_classes[dataset.lower()]
+
+    if name == 'transbench101_micro':
+        return search_space_cls(dataset=dataset)
+
+    # TODO_COMPETITION: Change all search space initializers to have this signature
+    return search_space_cls(n_classes=n_classes)
+    
