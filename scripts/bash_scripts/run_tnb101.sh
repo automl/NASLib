4a4241ac8d88bf4de8066d154235d833f891e5e1
rheasukthanker <20137590+rheasukthanker@users.noreply.github.com>
2022-04-06 09:52:43 +0200

    iterate over all datasets

--- scripts/bash_scripts/run_tnb101.sh
+++ scripts/bash_scripts/run_tnb101.sh
@@ -14,7 +14,7 @@ out_dir=$s3_folder\_$start_seed
 
 # search space / data:
 search_space=transbench101_micro
-dataset=class_scene
+datasets=(jigsaw class_object class_scene)
 
 # other variables:
 trials=100
@@ -22,23 +22,29 @@ end_seed=$(($start_seed + $trials - 1))
 test_size=100
 
 # create config files
-for i in $(seq 0 $((${#predictors[@]}-1)) )
+for dataset in ${datasets[@]}
 do
+ for i in $(seq 0 $((${#predictors[@]}-1)) )
+ do
     predictor=${predictors[$i]}
     experiment_type=${experiment_types[$i]}
-    python $base_file/create_configs.py --predictor $predictor --experiment_type $experiment_type \
-    --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
-    --dataset=$dataset --config_type predictor --search_space $search_space
+    python $base_file/create_configs.py --predictor $predictor --test_size $test_size \
+	    --start_seed $start_seed --trials $trials --out_dir $out_dir --dataset=$dataset \
+	    --search_space $search_space
+ done
 done
 
 # run experiments
 for t in $(seq $start_seed $end_seed)
 do
+ for dataset in ${datasets[@]}
+ do
     for predictor in ${predictors[@]}
     do
         config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
         echo ================running $predictor trial: $t =====================
         python naslib/runners/runner.py --config-file $config_file
     done
+ done
 done
 
