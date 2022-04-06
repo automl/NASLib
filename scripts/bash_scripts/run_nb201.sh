3614af2f14ac5686b99117120f10c7460096cd57
rheasukthanker <20137590+rheasukthanker@users.noreply.github.com>
2022-04-06 09:51:38 +0200

    iterate over all datasets

--- scripts/bash_scripts/run_nb201.sh
+++ scripts/bash_scripts/run_nb201.sh
@@ -11,9 +11,9 @@ base_file=scripts
 s3_folder=p201_c10
 out_dir=$s3_folder\_$start_seed
 
-# search space / data:
+# search space / datasets:
 search_space=nasbench201
-dataset=cifar10
+datasets=(cifar10 cifar100 ImageNet16-120)
 
 # other variables:
 trials=1
@@ -21,20 +21,26 @@ end_seed=$(($start_seed + $trials - 1))
 test_size=5
 
 # create config files
-for predictor in ${predictors[@]}
+for dataset in ${datasets[@]}
 do
+ for predictor in ${predictors[@]}
+ do
     python $base_file/create_configs.py --predictor $predictor \
     --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
     --dataset=$dataset --search_space $search_space
+ done  
 done
 
 # run experiments
 for t in $(seq $start_seed $end_seed)
 do
-    for predictor in ${predictors[@]}
+    for dataset in ${datasets[@]}
     do
+       for predictor in ${predictors[@]}
+       do
         config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
         echo ================running $predictor trial: $t =====================
         python naslib/runners/runner.py --config-file $config_file
+       done
     done
 done
