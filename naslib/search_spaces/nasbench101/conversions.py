4ecbc34b462f11997b81e3b8f7868e9469d72143
Arjun <arjun.krishnakumar@students.uni-freiburg.de>
2022-04-23 20:42:30 +0200

    Fix inplace ReLUs in NASBench101 model

--- naslib/search_spaces/nasbench101/conversions.py
+++ naslib/search_spaces/nasbench101/conversions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch.nn as nn
 
 from archai.algos.nasbench101 import model_builder
 from naslib.search_spaces.nasbench101.primitives import ModelWrapper
@@ -12,8 +13,29 @@ NUM_VERTICES = 7
 
 all_ops = ["input", "output", "maxpool3x3", "conv1x1-bn-relu", "conv3x3-bn-relu"]
 
+def get_children(model):
+    children = list(model.children())
+    all_children = []
+    if children == []:
+        return model
+    else:
+       for child in children:
+            try:
+                all_children.extend(get_children(child))
+            except TypeError:
+                all_children.append(get_children(child))
+
+    return all_children
+
 def convert_spec_to_model(spec):
     model = model_builder.build(spec['matrix'], spec['ops'])
+
+    all_leaf_modules = get_children(model)
+    inplace_relus = [module for module in all_leaf_modules if (isinstance(module, nn.ReLU) and module.inplace == True) ]
+
+    for relu in inplace_relus:
+        relu.inplace = False
+
     model_wrapper = ModelWrapper(model)
     return model_wrapper
 
