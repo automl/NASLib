77129ac450b61ab3076ce6c3d96796ffe43fc4e0
Arjun <arjun.krishnakumar@students.uni-freiburg.de>
2022-05-02 11:57:24 +0200

    Add script to test running the code on the cluster

---
+++ scripts/cluster/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p alldlc_gpu-rtx2080 #,ml_gpu-rtx2080 #ml_gpu-rtx2080     # bosch_gpu-rtx2080    #alldlc_gpu-rtx2080     # partition (queue)
+#SBATCH -o logs/%x.%A-%a.%N.out       # STDOUT  %A will be replaced by the SLURM_ARRAY_JOB_ID value
+#SBATCH -e logs/%x.%A-%a.%N.err       # STDERR  %A will be replaced by the SLURM_ARRAY_JOB_ID value
+#SBATCH -a 1 # array size
+
+echo "Workingdir: $PWD";
+echo "Started at $(date)";
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+start=`date +%s`
+
+python naslib/runners/runner.py --config-file configs/predictors/nasbench201-9000/cifar10/config_epe_nas_9000.yaml
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo Runtime: $runtime
