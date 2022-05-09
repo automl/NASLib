9f1012e8440d4ee4b343dacb762fcbd31651fad0
Arjun <arjun.krishnakumar.mec@gmail.com>
2022-05-09 16:57:28 +0200

    Add adjacency encoding for transnasbench101

--- naslib/predictors/utils/encodings.py
+++ naslib/predictors/utils/encodings.py
@@ -23,7 +23,21 @@ one_hot_nasbench201 = [[1,0,0,0,0],
 OPS = ['avg_pool_3x3', 'nor_conv_1x1', 'nor_conv_3x3', 'none', 'skip_connect']
 NUM_OPS = len(OPS)
 
-    
+one_hot_transnasbench201 = [[1,0,0,0],
+                            [0,1,0,0],
+                            [0,0,1,0],
+                            [0,0,0,1]]
+
+TRANS_OPS = ['Identity', 'Zero', 'ReLUConvBN3x3', 'ReLUConvBN1x1']
+TRANS_NUM_OPS = len(TRANS_OPS)
+
+def encode_adjacency_one_hot_transbench(arch):
+    encoding = arch.get_op_indices()
+    one_hot = []
+    for e in encoding:
+        one_hot = [*one_hot, *one_hot_transnasbench201[e]]
+    return one_hot
+
 def encode_adjacency_one_hot(arch):
     
     encoding = arch.get_op_indices()
@@ -184,6 +198,13 @@ def encode_seminas_nasbench201(arch):
 
     return dic
 
+def encode_transnasbench101(arch, encoding_type='adjacency_one_hot'):
+    if encoding_type == 'adjacency_one_hot':
+        return encode_adjacency_one_hot_transbench(arch)
+    else:
+        logger.info('{} is not yet supported as a predictor encoding'.format(encoding_type))
+        raise NotImplementedError()
+
 def encode_201(arch, encoding_type='adjacency_one_hot'):
         
     if encoding_type == 'adjacency_one_hot':
@@ -215,5 +236,7 @@ def encode(arch, encoding_type='adjacency_one_hot', ss_type=None):
         return encode_201(arch, encoding_type=encoding_type)
     elif ss_type == 'nasbench301':
         return encode_darts(arch, encoding_type=encoding_type)
+    elif ss_type == 'transbench101_micro':
+        return encode_transnasbench101(arch, encoding_type=encoding_type)
     else:
         raise NotImplementedError('{} is not yet supported for encodings'.format(ss_type))
