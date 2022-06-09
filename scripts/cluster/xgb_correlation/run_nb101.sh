8460bde6a1d81b54c5b2f60cbb3278303b440bbc
Arjun <arjun.krishnakumar.mec@gmail.com>
2022-06-09 15:01:06 +0200

    Add cluster scripts for xgb correlation experiments

--- scripts/cluster/xgb_correlation/run_nb101.sh
+++ scripts/cluster/xgb_correlation/run_nb101.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-train_sizes=(1000) #(10 15 23 36 56 87 135 209 323 500)
+train_sizes=(5 8 14 24 42 71 121 205 347 589 1000) #(1000) #(10 15 23 36 56 87 135 209 323 500)
 searchspace=nasbench101
 datasets=(cifar10)
 start_seed=9000
@@ -18,7 +18,7 @@ for dataset in "${datasets[@]}"
 do
     for size in "${train_sizes[@]}"
     do
-        sbatch ./scripts/cluster/xgb_correlation/run.sh $searchspace $dataset $size $start_seed $experiment <<< "y"
+        sbatch ./scripts/cluster/xgb_correlation/run.sh $searchspace $dataset $size $start_seed $experiment --bosch
     done
 
     echo ""
