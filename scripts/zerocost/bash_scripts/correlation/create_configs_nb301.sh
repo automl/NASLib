a9b07b1820ddf4bc7bb6499e8396792b5c3c821b
Abhash Jha <jhaa@informatik.uni-freiburg.de>
2022-10-05 17:23:20 +0200

    Removed Duplicate zerocost folder

--- scripts/zerocost/bash_scripts/correlation/create_configs_nb301.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-searchspace=nasbench301
-datasets=(cifar10 svhn scifar100 ninapro)
-
-for dataset in "${datasets[@]}"
-do
-    scripts/bash_scripts/correlation/create_configs.sh $searchspace $dataset 9000
-done
