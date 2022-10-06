d1a00388a6644e4c48ac3435058ed291bf087325
abhash-er <abhashgenuineacc11@gmail.com>
2022-10-06 13:15:56 +0200

    Added functions to convert op indices to naslib obejcts for TransNAS

--- naslib/search_spaces/transbench101/conversions.py
+++ naslib/search_spaces/transbench101/conversions.py
@@ -1,4 +1,8 @@
+import torch.nn as nn
 from naslib.search_spaces.core.graph import Graph
+from naslib.search_spaces.nasbench101.conversions import get_children
+from naslib.search_spaces.nasbench101.primitives import ModelWrapper
+from naslib.search_spaces.transbench101.tnb101.model_builder import create_model
 
 """
 There are three representations
@@ -23,6 +27,32 @@ def convert_naslib_to_op_indices(naslib_object):
         ops.append(cell.edges[i, j]['op'].get_op_name)
 
     return [OP_NAMES.index(name) for name in ops]
+
+
+def _wrap_model(model):
+    all_leaf_modules = get_children(model)
+    inplace_relus = [module for module in all_leaf_modules if (isinstance(module, nn.ReLU) and module.inplace == True)]
+
+    for relu in inplace_relus:
+        relu.inplace = False
+
+    model_wrapper = ModelWrapper(model)
+
+    return model_wrapper
+
+
+def convert_op_indices_macro_to_model(op_indices, task):
+    arch_str = convert_op_indices_macro_to_str(op_indices)
+    model = create_model(arch_str, task)
+
+    return _wrap_model(model)
+
+
+def convert_op_indices_micro_to_model(op_indices, task):
+    arch_str = convert_op_indices_micro_to_str(op_indices)
+    model = create_model(arch_str, task)
+
+    return _wrap_model(model)
 
 
 def convert_op_indices_to_naslib(op_indices, naslib_object):
