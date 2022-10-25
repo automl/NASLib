70e8951e5fb595f1b0061434272d221e8428f5a8
Abhash Jha <jhaa@informatik.uni-freiburg.de>
2022-10-25 14:59:05 +0200

    refactored supported optimizer

--- naslib/runners/bbo/runner.py
+++ naslib/runners/bbo/runner.py
@@ -37,15 +37,11 @@ if hasattr(config.search, 'use_zc_api'):
     zc_api = get_zc_benchmark_api(config.search_space, config.dataset)
 
 supported_optimizers = {
-    'rs': RandomSearch(config),
-    're': RegularizedEvolution(config),
-    'bananas': Bananas(config),
-    'bananas_zc': Bananas(config),
-    'bananas_zc_api': Bananas(config, zc_api=zc_api),
-    'npenas': Npenas(config),
-    'npenas_zc': Npenas(config),
-    'npenas_zc_api': Npenas(config, zc_api=zc_api),
-    'ls': LocalSearch(config),
+    # 'rs': RandomSearch(config),
+    # 're': RegularizedEvolution(config),
+    'bananas': Bananas(config, zc_api=zc_api),
+    'npenas': Npenas(config, zc_api=zc_api),
+    # 'ls': LocalSearch(config),
 }
 
 supported_search_spaces = {
