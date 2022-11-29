edf851c25dfd23d856e843c2e67c27d07f1dc800
lukas <lukas.j.m.strack@gmail.com>
2022-11-29 10:51:20 +0100

    add plotting routine for alpha weights

--- examples/run_darts.py
+++ examples/run_darts.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from naslib.defaults.trainer import Trainer
 from naslib.optimizers import DARTSOptimizer, GDASOptimizer, RandomSearch
@@ -7,20 +6,16 @@ from naslib.search_spaces import DartsSearchSpace, SimpleCellSearchSpace
 from naslib.utils import set_seed, setup_logger, get_config_from_args
 
 config = get_config_from_args()  # use --help so see the options
-config.search.batch_size = 256
-config.search.epochs = 3
-config.save_arch_weights = True
-config.save_arch_weights_path = f"{os.getcwd()}/save_arch"
 set_seed(config.seed)
 
 logger = setup_logger(config.save + "/log.log")
 logger.setLevel(logging.INFO)  # default DEBUG is very verbose
 
-search_space = SimpleCellSearchSpace()  # DartsSearchSpace()  # use SimpleCellSearchSpace() for less heavy search
+search_space = DartsSearchSpace()  # use SimpleCellSearchSpace() for less heavy search
 
 optimizer = DARTSOptimizer(config)
 optimizer.adapt_search_space(search_space)
 
 trainer = Trainer(optimizer, config)
 trainer.search()  # Search for an architecture
-# trainer.evaluate()  # Evaluate the best architecture
+trainer.evaluate()  # Evaluate the best architecture
