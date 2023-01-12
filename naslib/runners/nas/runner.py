73c70c880647dddea3f0066e5e7ad7a61d5c29d1
shakibamrd <shakiba.moradian@yahoo.com>
2023-01-12 22:10:51 +0100

    remove auxiliry head when running nb301

--- naslib/runners/nas/runner.py
+++ naslib/runners/nas/runner.py
@@ -47,7 +47,7 @@ supported_optimizers = {
 supported_search_spaces = {
     'nasbench101': NasBench101SearchSpace(),
     'nasbench201': NasBench201SearchSpace(),
-    'nasbench301': NasBench301SearchSpace(),
+    'nasbench301': NasBench301SearchSpace(auxiliary=False),
     'nlp': NasBenchNLPSearchSpace(),
     'transbench101_micro': TransBench101SearchSpaceMicro(config.dataset),
     'transbench101_macro': TransBench101SearchSpaceMacro(),
