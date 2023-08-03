44f58415ade73b57dd7f5dd3c0a530490dd25dd6
gurizab <gurizabergja96@gmail.com>
2023-08-03 19:10:55 +0200

    add npenas docstrings and add it to docs

---
+++ NASLib_docs/docs/optimizers/discrete/npenas.md
@@ -0,0 +1,1 @@
+::: naslib.optimizers.discrete.npenas.optimizer.Npenas
--- naslib/optimizers/discrete/npenas/optimizer.py
+++ naslib/optimizers/discrete/npenas/optimizer.py
@@ -19,16 +19,35 @@ from naslib.search_spaces.core.query_metrics import Metric
 from naslib.utils import AttrDict, count_parameters_in_MB, get_train_val_loaders
 from naslib.utils.log import log_every_n_seconds
 
-
 logger = logging.getLogger(__name__)
 
 
 class Npenas(MetaOptimizer):
+    """
+    Implements the Npenas optimizer.
+
+    Npenas is a Neural Predictor Guided Evolution for Neural Architecture Search
+    optimization algorithm which uses machine learning to guide the search process of
+    neural architecture search (NAS). This algorithm employs predictive
+    models, acquisition functions, and zero-cost proxies to speed up the
+    search process.
+
+    Attributes:
+        config (obj): Configuration object containing setup parameters.
+        zc_api (obj, optional): Zero cost API object to query zero-cost proxies.
+    """
 
     # training the models is not implemented
     using_step_function = False
 
     def __init__(self, config, zc_api=None):
+        """
+        Constructor of Npenas optimizer.
+
+        Args:
+            config (obj): Configuration object containing setup parameters.
+            zc_api (obj, optional): Zero cost API object to query zero-cost proxies.
+        """
         super().__init__()
         self.config = config
         self.epochs = config.search.epochs
@@ -50,17 +69,28 @@ class Npenas(MetaOptimizer):
         self.next_batch = []
         self.history = torch.nn.ModuleList()
 
-        self.zc = config.search.zc_ensemble if hasattr(config.search, 'zc_ensemble') else None 
+        self.zc = config.search.zc_ensemble if hasattr(config.search, 'zc_ensemble') else None
         self.semi = "semi" in self.predictor_type
         self.zc_api = zc_api
         self.use_zc_api = config.search.use_zc_api if hasattr(
             config.search, 'use_zc_api') else False
         self.zc_names = config.search.zc_names if hasattr(
-            config.search, 'zc_names') else None 
+            config.search, 'zc_names') else None
         self.zc_only = config.search.zc_only if hasattr(
             config.search, 'zc_only') else False
 
     def adapt_search_space(self, search_space, scope=None, dataset_api=None):
+        """
+        Adapts the search space for optimization.
+
+        Args:
+            search_space (obj): The search space object that contains all possible architectures.
+            scope (str, optional): Defines the scope of the search space for the optimizer.
+            dataset_api (obj, optional): API for querying dataset specific metrics.
+
+        Raises:
+            AssertionError: If search_space is not queryable.
+        """
         assert (
             search_space.QUERYABLE
         ), "Npenas is currently only implemented for benchmarks."
@@ -77,14 +107,29 @@ class Npenas(MetaOptimizer):
             self.unlabeled = []
 
     def get_zero_cost_predictors(self):
+        """
+        Creates and returns a dictionary of zero cost predictors based on the `zc_names` configuration.
+
+        Returns:
+            dict: A dictionary where keys are zero cost predictor names and values are the predictor objects.
+        """
         return {zc_name: ZeroCost(method_type=zc_name) for zc_name in self.zc_names}
 
     def query_zc_scores(self, arch):
+        """
+        Queries and returns the zero-cost scores for a given architecture.
+
+        Args:
+            arch (obj): The architecture to query.
+
+        Returns:
+            dict: A dictionary where keys are zero cost predictor names and values are the corresponding scores.
+        """
         zc_scores = {}
         zc_methods = self.get_zero_cost_predictors()
         arch_hash = arch.get_hash()
         for zc_name, zc_method in zc_methods.items():
-            
+
             if self.use_zc_api and str(arch_hash) in self.zc_api:
                 score = self.zc_api[str(arch_hash)][zc_name]['score']
             else:
@@ -101,6 +146,12 @@ class Npenas(MetaOptimizer):
         return zc_scores
 
     def _set_scores(self, model):
+        """
+        Sets the accuracy and zero-cost scores for a given model.
+
+        Args:
+            model (obj): The model to set scores for.
+        """
 
         if self.use_zc_api and str(model.arch_hash) in self.zc_api:
             model.accuracy = self.zc_api[str(model.arch_hash)]['val_accuracy']
@@ -108,7 +159,7 @@ class Npenas(MetaOptimizer):
             model.accuracy = model.arch.query(
                 self.performance_metric, self.dataset, dataset_api=self.dataset_api
             )
-        
+
         if self.zc and len(self.train_data) <= self.max_zerocost:
             model.zc_scores = self.query_zc_scores(model.arch)
 
@@ -116,6 +167,12 @@ class Npenas(MetaOptimizer):
         self._update_history(model)
 
     def _sample_new_model(self):
+        """
+        Samples and returns a new model from the search space.
+
+        Returns:
+            obj: The new sampled model.
+        """
         model = torch.nn.Module()
         model.arch = self.search_space.clone()
         model.arch.sample_random_architecture(
@@ -126,26 +183,45 @@ class Npenas(MetaOptimizer):
             model.arch.parse()
 
         return model
-    
+
     def _get_train(self):
+        """
+        Retrieves the training data.
+
+        Returns:
+            list: The list of architectures used for training.
+            list: The corresponding list of accuracies for each architecture.
+        """
         xtrain = [m.arch for m in self.train_data]
         ytrain = [m.accuracy for m in self.train_data]
         return xtrain, ytrain
 
-    
     def _get_ensemble(self):
+        """
+        Creates and returns an ensemble of predictors.
+
+        Returns:
+            obj: The ensemble object.
+        """
         ensemble = Ensemble(num_ensemble=self.num_ensemble,
                             ss_type=self.ss_type,
                             predictor_type=self.predictor_type,
                             zc=self.zc,
                             zc_only=self.zc_only,
                             config=self.config)
-        
+
         return ensemble
 
     def _get_new_candidates(self, ytrain):
-        candidates = []
+        """
+        Returns a list of candidate architectures by mutating the best architectures.
 
+        Args:
+            ytrain (list): The list of accuracies of the architectures used for training.
+
+        Returns:
+            list: A list of candidate architectures.
+        """
         # mutate the k best architectures by x
         best_arch_indices = np.argsort(ytrain)[-self.num_arches_to_mutate:]
         best_archs = [self.train_data[i].arch for i in best_arch_indices]
@@ -168,6 +244,12 @@ class Npenas(MetaOptimizer):
         return candidates
 
     def new_epoch(self, epoch):
+        """
+        Conducts one epoch of the optimization process.
+
+        Args:
+            epoch (int): The current epoch number.
+        """
 
         if epoch < self.num_init:
             model = self._sample_new_model()
@@ -182,16 +264,16 @@ class Npenas(MetaOptimizer):
                     # create unlabeled data and pass it to the predictor
                     while len(self.unlabeled) < len(xtrain):
                         model = self._sample_new_model()
-                        
+
                         if self.zc and len(self.train_data) <= self.max_zerocost:
                             model.zc_scores = self.query_zc_scores(model.arch)
 
                         self.unlabeled.append(model)
-                    
+
                     ensemble.set_pre_computations(
                         unlabeled=[m.arch for m in self.unlabeled]
                     )
-                
+
                 if self.zc and len(self.train_data) <= self.max_zerocost:
                     # pass the zero-cost scores to the predictor
                     train_info = {'zero_cost_scores': [m.zc_scores for m in self.train_data]}
@@ -216,14 +298,24 @@ class Npenas(MetaOptimizer):
             # train the next architecture chosen by the neural predictor
             model = self.next_batch.pop()
             self._set_scores(model)
-    
+
     def _get_best_candidates(self, candidates, acq_fn):
-        
+        """
+        Returns the best candidate architectures based on the acquisition function.
+
+        Args:
+            candidates (list): A list of candidate architectures.
+            acq_fn (function): The acquisition function.
+
+        Returns:
+            list: A list of the best candidate architectures.
+        """
+
         if self.zc and len(self.train_data) <= self.max_zerocost:
             for model in candidates:
                 model.zc_scores = self.query_zc_scores(model.arch_hash, self.zc_names, self.zc_api)
 
-            values = [acq_fn(model.arch, [{'zero_cost_scores' : model.zc_scores}]) for model in candidates]
+            values = [acq_fn(model.arch, [{'zero_cost_scores': model.zc_scores}]) for model in candidates]
         else:
             values = [acq_fn(model.arch) for model in candidates]
 
@@ -233,6 +325,13 @@ class Npenas(MetaOptimizer):
         return choices
 
     def _update_history(self, child):
+        """
+        Updates the history of the optimizer by replacing the worst model in history with the given model
+        if the given model's accuracy is better.
+
+        Args:
+            child (obj): The model to potentially add to the history.
+        """
         if len(self.history) < 100:
             self.history.append(child)
         else:
@@ -242,6 +341,15 @@ class Npenas(MetaOptimizer):
                     break
 
     def train_statistics(self, report_incumbent=True):
+        """
+        Returns the training statistics of the best architecture or the last trained one.
+
+        Args:
+            report_incumbent (bool, optional): Whether to return the statistics of the best architecture. Defaults to True.
+
+        Returns:
+            tuple: A tuple containing the training accuracy, validation accuracy, test accuracy, and training time of the architecture.
+        """
         if report_incumbent:
             best_arch = self.get_final_architecture()
         else:
@@ -264,7 +372,7 @@ class Npenas(MetaOptimizer):
             )
         else:
             return (
-                -1, 
+                -1,
                 best_arch.query(
                     Metric.VAL_ACCURACY, self.dataset, dataset_api=self.dataset_api
                 ),
@@ -274,9 +382,15 @@ class Npenas(MetaOptimizer):
                 best_arch.query(
                     Metric.TRAIN_TIME, self.dataset, dataset_api=self.dataset_api
                 ),
-            ) 
+            )
 
     def test_statistics(self):
+        """
+        Returns the test statistics of the final architecture.
+
+        Returns:
+            int: The raw test metric of the final architecture.
+        """
         best_arch = self.get_final_architecture()
         if self.search_space.space_name != "nasbench301":
             return best_arch.query(Metric.RAW, self.dataset, dataset_api=self.dataset_api)
@@ -284,21 +398,53 @@ class Npenas(MetaOptimizer):
             return -1
 
     def get_final_architecture(self):
+        """
+        Returns the final/best architecture from the history.
+
+        Returns:
+            obj: The final/best architecture.
+        """
         return max(self.history, key=lambda x: x.accuracy).arch
 
     def get_op_optimizer(self):
+        """
+        Method to get the optimizer of the operation. Not implemented in this class.
+
+        Raises:
+            NotImplementedError: Always, as this method is not implemented.
+        """
         raise NotImplementedError()
 
     def get_checkpointables(self):
+        """
+        Returns the models that should be checkpointed.
+
+        Returns:
+            dict: A dictionary with 'model' as the key and the history of models as the value.
+        """
         return {"model": self.history}
 
     def get_model_size(self):
+        """
+        Returns the size of the model.
+
+        Returns:
+            float: The size of the model in MB.
+        """
         return count_parameters_in_MB(self.history)
-    
+
     def get_arch_as_string(self, arch):
+        """
+        Returns a string representation of the given architecture.
+
+        Args:
+            arch (obj): The architecture to convert to string.
+
+        Returns:
+            str: The string representation of the architecture.
+        """
         if self.search_space.get_type() == 'nasbench301':
             str_arch = str(list((list(arch[0]), list(arch[1]))))
         else:
             str_arch = str(arch)
         return str_arch
-        
