5f6232c6cadefe7da15b807bbbb6251934a7efbd
gurizab <gurizabergja96@gmail.com>
2023-08-30 13:29:37 +0200

    add docstrings for drnas optimizer

--- naslib/optimizers/oneshot/drnas/optimizer.py
+++ naslib/optimizers/oneshot/drnas/optimizer.py
@@ -19,27 +19,42 @@ logger = logging.getLogger(__name__)
 class DrNASOptimizer(DARTSOptimizer):
     """
     Implementation of DrNAS optimizer introduced in the paper
-        DrNAS: Dirichlet Neural Architecture Search (ICLR2021)
-    note: many functions are similar to the DARTS optimizer so it makes sense to inherit this class directly
-    from DARTSOptimizer instead of MetaOptimizer
+    DrNAS: Dirichlet Neural Architecture Search (ICLR2021).
+
+    Note: Many functions are similar to the DARTS optimizer, so this class is inherited directly
+    from DARTSOptimizer instead of MetaOptimizer.
     """
 
     @staticmethod
     def sample_alphas(edge):
+        """
+        Sample architecture weights (alphas) using the Dirichlet distribution parameterized by beta.
+
+        Args:
+            edge: The edge in the computation graph where the sample architecture weights are set.
+        """
         beta = F.elu(edge.data.alpha) + 1
         weights = torch.distributions.dirichlet.Dirichlet(beta).rsample()
         edge.data.set("sampled_arch_weight", weights, shared=True)
 
     @staticmethod
     def remove_sampled_alphas(edge):
+        """
+        Remove sampled architecture weights (alphas) from the edge's data.
+
+        Args:
+            edge: The edge in the computation graph where the sample architecture weights are to be removed.
+        """
         if edge.data.has("sampled_arch_weight"):
             edge.data.remove("sampled_arch_weight")
 
     @staticmethod
     def update_ops(edge):
         """
-        Function to replace the primitive ops at the edges
-        with the DrNAS specific DrNASMixedOp.
+        Replace the primitive operations at the edge with the DrNAS-specific DrNASMixedOp.
+
+        Args:
+            edge: The edge in the computation graph where the operations are to be replaced.
         """
         primitives = edge.data.op
         edge.data.set("op", DrNASMixedOp(primitives))
@@ -60,10 +75,21 @@ class DrNASOptimizer(DARTSOptimizer):
         **kwargs
     ):
         """
-        Initialize a new instance.
+        Initialize a new instance of the DrNASOptimizer class.
 
         Args:
-
+            learning_rate (float): Learning rate for operation weights.
+            momentum (float): Momentum for the optimizer.
+            weight_decay (float): Weight decay for operation weights.
+            grad_clip (int): Gradient clipping threshold.
+            unrolled (bool): Whether to use unrolled optimization.
+            arch_learning_rate (float): Learning rate for architecture weights.
+            arch_weight_decay (float): Weight decay for architecture weights.
+            epochs (int): Total number of training epochs.
+            op_optimizer (str): The optimizer type for operation weights. E.g., 'SGD'
+            arch_optimizer (str): The optimizer type for architecture weights. E.g., 'Adam'
+            loss_criteria (str): Loss criteria. E.g., 'CrossEntropyLoss'
+            **kwargs: Additional keyword arguments.
         """
         super().__init__(learning_rate, momentum, weight_decay, grad_clip, unrolled, arch_learning_rate, arch_weight_decay, op_optimizer, arch_optimizer, loss_criteria)
         self.reg_type = "kl"
@@ -73,12 +99,22 @@ class DrNASOptimizer(DARTSOptimizer):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def new_epoch(self, epoch):
+        """
+        Perform any operations needed at the start of a new epoch.
+
+        Args:
+            epoch (int): Current epoch number.
+        """
         super().new_epoch(epoch)
 
     def adapt_search_space(self, search_space, dataset, scope=None):
         """
-        Same as in darts with a different mixop.
-        If you want to checkpoint the dirichlet 'concentration' parameter (beta) add it to the buffer here.
+        Adapt the search space for architecture search.
+
+        Args:
+            search_space: The initial search space.
+            dataset: The dataset for training/validation.
+            scope: Scope to update in the search space. Default is None.
         """
         super().adapt_search_space(search_space, dataset, scope)
         self.anchor = Dirichlet(
@@ -88,6 +124,16 @@ class DrNASOptimizer(DARTSOptimizer):
         )
 
     def step(self, data_train, data_val):
+        """
+        Perform a single optimization step for both architecture and operation weights.
+
+        Args:
+            data_train (tuple): Training data as a tuple of inputs and labels.
+            data_val (tuple): Validation data as a tuple of inputs and labels.
+
+        Returns:
+            tuple: Logits for training data, logits for validation data, loss for training data, loss for validation data.
+        """
         input_train, target_train = data_train
         input_val, target_val = data_val
 
@@ -142,6 +188,12 @@ class DrNASOptimizer(DARTSOptimizer):
         return logits_train, logits_val, train_loss, val_loss
 
     def _get_kl_reg(self):
+        """
+        Calculate the KL regularization term based on the Dirichlet distribution.
+
+        Returns:
+            torch.Tensor: The KL regularization term.
+        """
         cons = (
             F.elu(torch.nn.utils.parameters_to_vector(self.architectural_weights)) + 1
         )
@@ -151,6 +203,12 @@ class DrNASOptimizer(DARTSOptimizer):
         return kl_reg
 
     def get_final_architecture(self):
+        """
+        Retrieve the final, discretized architecture based on current architectural weights.
+
+        Returns:
+            Graph: The final architecture in graph representation.
+        """
         logger.info(
             "Arch weights before discretization: {}".format(
                 [a for a in self.architectural_weights]
@@ -173,23 +231,56 @@ class DrNASOptimizer(DARTSOptimizer):
 
 
 class DrNASMixedOp(MixedOp):
+    """
+    Specialized MixedOp for DrNAS. Handles the sampled architecture weights (alphas).
+
+    """
     def __init__(self, primitives, min_cuda_memory=False):
         """
-        Initialize the mixed ops
+        Initialize a new instance of the DrNASMixedOp class.
 
         Args:
-            primitives (list): The primitive operations to sample from.
+            primitives (list): List of primitive operations to sample from.
+            min_cuda_memory (bool): Whether to minimize CUDA memory usage. Default is False.
         """
         super().__init__(primitives)
         self.min_cuda_memory = min_cuda_memory
 
     def get_weights(self, edge_data):
+        """
+        Retrieve the sampled architecture weights from the edge data.
+
+        Args:
+            edge_data: The data associated with an edge in the computational graph.
+
+        Returns:
+            torch.Tensor: The sampled architecture weights.
+        """
         return edge_data.sampled_arch_weight
 
     def process_weights(self, weights):
+        """
+        Process the weights if any additional operations are needed.
+
+        Args:
+            weights (torch.Tensor): The architecture weights.
+
+        Returns:
+            torch.Tensor: The processed architecture weights.
+        """
         return weights
 
     def apply_weights(self, x, weights):
+        """
+        Apply the architecture weights to the primitive operations.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+            weights (torch.Tensor): The architecture weights.
+
+        Returns:
+            torch.Tensor: The output tensor after applying the architecture weights.
+        """
         weighted_sum = sum(
             w * op(x, None)
             for w, op in zip(weights, self.primitives)
